6baba488b956c4010a015aa1189c8e9bf95075bf
extrasauceontop <msuchemba@gmail.com>
2022-03-03 11:39:28 -0600

    scrape

--- scrape.py
+++ scrape.py
@@ -26,7 +26,7 @@ def get_driver(url, class_name, driver=None):
             driver = SgChrome(
                 executable_path=ChromeDriverManager().install(),
                 user_agent=user_agent,
-                is_headless=False,
+                is_headless=True,
             ).driver()
 
             driver.get(url)
