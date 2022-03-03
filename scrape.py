75ba22d77ded25ce236267a58dfc3909c9cfa488
extrasauceontop <msuchemba@gmail.com>
2022-03-03 16:17:41 -0600

    scrape

--- scrape.py
+++ scrape.py
@@ -3,12 +3,60 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from sgselenium.sgselenium import SgChrome
 from webdriver_manager.chrome import ChromeDriverManager
-from sgscrape import simple_scraper_pipeline as sp
 from sgrequests import SgRequests
+import json
+from sgscrape import simple_scraper_pipeline as sp
 from sgpostal.sgpostal import parse_address_intl
 import ssl
+import time
+from bs4 import BeautifulSoup as bs
 
 ssl._create_default_https_context = ssl._create_unverified_context
+session = SgRequests()
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+    "accept-language": "en-US,en;q=0.9,ar;q=0.8",
+}
+
+
+def get_page_urls():
+    start_url = "https://headquartersoffice.com/page/"
+    x = 0
+
+    final_links = []
+    while True:
+        x = x+1
+        url = start_url + str(x) + "/?s"
+
+        try:
+            response = session.get(url, headers=headers).text
+
+        except Exception:
+            print(url)
+            break
+        if "looks like nothing was found at this location" in response:
+            break
+
+        soup = bs(response, "html.parser")
+        testings = soup.find_all("h2", attrs={"class": "entry-title"})
+        for test in testings:
+            if test.find("a").text.strip()[0].isalpha() is False or test.find("a").text.strip()[0] == "(":
+                try:
+                    print(test.find("a").text.strip())
+                    print(test.find("a")["href"])
+                
+                except Exception:
+                    print(test.find("a")["href"])
+            
+                print("")
+                print("")
+
+        links = [h2_tag.find("a")["href"] for h2_tag in soup.find_all("h2", attrs={"class": "entry-title"}) if h2_tag.find("a").text.strip()[0].isalpha() is True or h2_tag.find("a").text.strip()[0] == "("]
+
+        for link in links:
+            final_links.append(link)
+
+    return final_links
 
 
 def get_driver(url, class_name, driver=None):
@@ -44,6 +92,101 @@ def get_driver(url, class_name, driver=None):
     
     driver.maximize_window()
     return driver
+
+
+def new_map_page(driver):
+    while True:
+        try:
+            element = driver.find_element_by_class_name("paginationjs-next.J-paginationjs-next").find_element_by_css_selector("a")
+            driver.execute_script("arguments[0].click();", element)
+            time.sleep(2)
+
+        except Exception as e:
+            print(e)
+            break
+
+    test = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
+
+    responses = []
+    for item in test:
+        if "base64" in item["name"] and "marker-list" in item["name"]:
+            response = session.get(item["name"]).json()
+            responses.append(response)
+    
+    for response in responses:
+        for location in response["meta"]:
+            locator_domain = "https://headquartersoffice.com/"
+            page_url = driver.current_url
+            location_name = location["title"]
+            latitude = location["lat"]
+            longitude = location["lng"]
+            
+            store_number = location["id"]
+            full_address = location["address"]
+
+            if "+" in full_address:
+                full_address = "".join(part + " " for part in full_address.split(" ")[1:])
+            
+            if latitude in full_address and longitude in full_address:
+                city = "<MISSING>"
+                address = "<MISSING>"
+                state = "<MISSING>"
+                zipp = "<MISSING>"
+                country_code = "<MISSING>"
+                full_address = "<MISSING>"
+            
+            if full_address != "<MISSING>":
+                print(full_address)
+                addr = parse_address_intl(full_address)
+                print(addr)
+                print("")
+                city = addr.city
+                if city is None:
+                    city = "<MISSING>"
+                
+                address_1 = addr.street_address_1
+                address_2 = addr.street_address_2
+
+                if address_1 is None and address_2 is None:
+                    address = "<MISSING>"
+                else:
+                    address = (str(address_1) + " " + str(address_2)).strip()
+                
+                state = addr.state
+                if state is None:
+                    state = "<MISSING>"
+                
+                zipp = addr.postcode
+                if zipp is None:
+                    zipp = "<MISSING>"
+                
+                country_code = addr.country
+                if country_code is None:
+                    country_code = "<MISSING>"
+
+            phone = "<MISSING>"
+
+            if page_url[-1] == "/":
+                page_url = page_url[:-1]
+            location_type = page_url.split("/")[-1]
+            hours = "<MISSING>"
+
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "store_number": store_number,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
 
 
 def old_map_page(driver):
@@ -140,13 +283,53 @@ def old_map_page(driver):
 
 
 def get_data():
-    page_url = "https://headquartersoffice.com/verizon/"
-    driver = get_driver(page_url, "inside-page-hero")
+    page_urls = get_page_urls()
+    x = 0
+    y = 0
+    for page_url in page_urls:
+        if page_url == page_urls[0]:
+            driver = get_driver(page_url, "inside-page-hero")
+        
+        else:
+            try:
+                driver.get(page_url)
+            
+            except Exception:
+                driver = get_driver(page_url, "inside-page-hero", driver=driver)
 
-    locations = old_map_page(driver)
+        response = driver.page_source
+        soup = bs(response, "html.parser")
 
-    for loc in locations:
-        yield loc
+        map_object = soup.find("div", attrs={"class": "wpgmza_map"})
+        if map_object is None:
+            continue
+        
+        else:
+            time.sleep(1)
+            test = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
+            
+            found = 0
+            for item in test:
+                if "base64" in item["name"] and "marker-list" in item["name"]:
+                    x = x+1
+                    locations = new_map_page(driver)
+                    found = 1
+                    
+                    for loc in locations:
+                        yield loc
+
+                    break
+
+
+            if found == 0:
+                locations = old_map_page(driver)
+
+                for loc in locations:
+                    yield loc
+                y = y+1
+
+    print(x)
+    print(y)
 
 
 def scrape():
