c803095cb94b8b0743c02ed2d3522a377a021e71
extrasauceontop <msuchemba@gmail.com>
2022-03-06 17:14:50 -0600

    scrape

--- scrape.py
+++ scrape.py
@@ -1,63 +1,25 @@
-from pydoc import stripid
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from sgselenium.sgselenium import SgChrome
 from webdriver_manager.chrome import ChromeDriverManager
-from sgrequests import SgRequests
-import json
+from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
 from sgscrape import simple_scraper_pipeline as sp
-from sgpostal.sgpostal import parse_address_intl
+import ast
 import ssl
-import time
-from bs4 import BeautifulSoup as bs
 
 ssl._create_default_https_context = ssl._create_unverified_context
-session = SgRequests()
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-    "accept-language": "en-US,en;q=0.9,ar;q=0.8",
-}
+search = DynamicGeoSearch(country_codes=[SearchableCountries.BRITAIN])
 
-
-def get_page_urls():
-    start_url = "https://headquartersoffice.com/page/"
-    x = 0
-
-    final_links = []
-    while True:
-        x = x+1
-        url = start_url + str(x) + "/?s"
-
-        try:
-            response = session.get(url, headers=headers).text
-
-        except Exception:
-            print(url)
-            break
-        if "looks like nothing was found at this location" in response:
-            break
-
-        soup = bs(response, "html.parser")
-        testings = soup.find_all("h2", attrs={"class": "entry-title"})
-        for test in testings:
-            if test.find("a").text.strip()[0].isalpha() is False or test.find("a").text.strip()[0] == "(":
-                try:
-                    print(test.find("a").text.strip())
-                    print(test.find("a")["href"])
-                
-                except Exception:
-                    print(test.find("a")["href"])
-            
-                print("")
-                print("")
-
-        links = [h2_tag.find("a")["href"] for h2_tag in soup.find_all("h2", attrs={"class": "entry-title"}) if h2_tag.find("a").text.strip()[0].isalpha() is True or h2_tag.find("a").text.strip()[0] == "("]
-
-        for link in links:
-            final_links.append(link)
-
-    return final_links
+hours_key_list = [
+    ["MonOpen", "MonClose"],
+    ["TueOpen", "TueClose"],
+    ["WedOpen", "WedClose"],
+    ["ThuOpen", "ThuClose"],
+    ["FriOpen", "FriClose"],
+    ["SatOpen", "SatClose"],
+    ["SunOpen", "SunClose"],
+]
 
 
 def get_driver(url, class_name, driver=None):
@@ -76,7 +38,6 @@ def get_driver(url, class_name, driver=None):
                 user_agent=user_agent,
                 is_headless=True,
             ).driver()
-
             driver.get(url)
 
             WebDriverWait(driver, 20).until(
@@ -90,248 +51,164 @@ def get_driver(url, class_name, driver=None):
                     "Make sure this ran with a Proxy, will fail without one"
                 )
             continue
-    
-    driver.maximize_window()
     return driver
 
 
-def new_map_page(driver):
-    locations = []
-    while True:
-        try:
-            element = driver.find_element_by_class_name("paginationjs-next.J-paginationjs-next").find_element_by_css_selector("a")
-            driver.execute_script("arguments[0].click();", element)
-            time.sleep(2)
-
-        except Exception as e:
-            break
-
-    test = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
-
-    responses = []
-    for item in test:
-        if "base64" in item["name"] and "marker-list" in item["name"]:
-            response = session.get(item["name"]).json()
-            responses.append(response)
-    
-    for response in responses:
-        for location in response["meta"]:
-            locator_domain = "https://headquartersoffice.com/"
-            page_url = driver.current_url
-            location_name = location["title"]
-            latitude = location["lat"]
-            longitude = location["lng"]
-            
-            store_number = location["id"]
-            full_address = location["address"]
-
-            if "+" in full_address:
-                full_address = "".join(part + " " for part in full_address.split(" ")[1:])
-            
-            if latitude in full_address and longitude in full_address:
-                city = "<MISSING>"
-                address = "<MISSING>"
-                state = "<MISSING>"
-                zipp = "<MISSING>"
-                country_code = "<MISSING>"
-                full_address = "<MISSING>"
-            
-            if full_address != "<MISSING>":
-                addr = parse_address_intl(full_address)
-                city = addr.city
-                if city is None:
-                    city = "<MISSING>"
-                
-                address_1 = addr.street_address_1
-                address_2 = addr.street_address_2
-
-                if address_1 is None and address_2 is None:
-                    address = "<MISSING>"
-                else:
-                    address = (str(address_1) + " " + str(address_2)).strip()
-                
-                state = addr.state
-                if state is None:
-                    state = "<MISSING>"
-                
-                zipp = addr.postcode
-                if zipp is None:
-                    zipp = "<MISSING>"
-                
-                country_code = addr.country
-                if country_code is None:
-                    country_code = "<MISSING>"
-
-            phone = "<MISSING>"
-
-            if page_url[-1] == "/":
-                page_url = page_url[:-1]
-            location_type = page_url.split("/")[-1]
-            hours = "<MISSING>"
-
-            locations.append({
-                "locator_domain": locator_domain,
-                "page_url": page_url,
-                "location_name": location_name,
-                "latitude": latitude,
-                "longitude": longitude,
-                "city": city,
-                "store_number": store_number,
-                "street_address": address,
-                "state": state,
-                "zip": zipp,
-                "phone": phone,
-                "location_type": location_type,
-                "hours": hours,
-                "country_code": country_code,
-            })
-    
-    return locations
-
-
-def old_map_page(driver):
-    locations = []
-    test = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
-    for item in test:
-        if "base64" in item["name"]:
-
-            session = SgRequests()
-            url = item["name"]
-            try:
-                response = session.get(url).json()
-            
-            except Exception:
-                continue
-
-            if "markers" in response.keys():
-                break
-    
-    try:
-        if response is None:
-            print(driver.current_url)
-            raise Exception
-    
-    except Exception:
-        print(driver.current_url)
-        raise Exception
-    for location in response["markers"]:
-        locator_domain = "https://headquartersoffice.com/"
-        page_url = driver.current_url
-        location_name = location["title"]
-        latitude = location["lat"]
-        longitude = location["lng"]
-        store_number = location["id"]
-        
-        phone = "<MISSING>"
-        for field in location["custom_field_data"]:
-            if "phone" in field["name"].lower():
-                phone = field["value"].replace("+", "")
-        
-        if page_url[-1] == "/":
-            page_url = page_url[:-1]
-        location_type = page_url.split("/")[-1]
-        hours = "<MISSING>"
-
-        full_address = "lost"
-        for field in location["custom_field_data"]:
-            if "address" in field["name"].lower():
-                full_address = field["value"].replace("+", "")
-        
-        if full_address == "lost":
-            full_address = location["address"]
-        
-        if "+" in full_address.split(" ")[0]:
-            full_address = "".join(part + " " for part in full_address.split(" ")[1:])
-        
-        if full_address != "lost":
-            addr = parse_address_intl(full_address)
-            city = addr.city
-            if city is None:
-                city = "<MISSING>"
-            
-            address_1 = addr.street_address_1
-            address_2 = addr.street_address_2
-
-            if address_1 is None and address_2 is None:
-                address = "<MISSING>"
-            else:
-                address = (str(address_1) + " " + str(address_2)).strip()
-            
-            state = addr.state
-            if state is None:
-                state = "<MISSING>"
-            
-            zipp = addr.postcode
-            if zipp is None:
-                zipp = "<MISSING>"
-            
-            country_code = addr.country
-            if country_code is None:
-                country_code = "<MISSING>"
-
-        else:
-            city = "<MISSING>"
-            address = "<MISSING>"
-            state = "<MISSING>"
-            zipp = "<MISSING>"
-            country_code = "<MISSING>"
-
-        locations.append({
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "store_number": store_number,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        })
-    
-    return locations
-
-
 def get_data():
-    page_urls = get_page_urls()
-    x = 0
-    y = 0
-    for page_url in page_urls:
-        if page_url == page_urls[0]:
-            driver = get_driver(page_url, "inside-page-hero")
-        
-        else:
+    search = DynamicGeoSearch(country_codes=[SearchableCountries.BRITAIN])
+
+    hours_key_list = [
+        ["MonOpen", "MonClose"],
+        ["TueOpen", "TueClose"],
+        ["WedOpen", "WedClose"],
+        ["ThuOpen", "ThuClose"],
+        ["FriOpen", "FriClose"],
+        ["SatOpen", "SatClose"],
+        ["SunOpen", "SunClose"],
+    ]
+    driver = get_driver("https://www.schuh.co.uk/stores/", "secondLine")
+
+    for search_lat, search_lon in search:
+        while True:
             try:
-                driver.get(page_url)
-            
+                data = driver.execute_async_script(
+                    r"""
+                    var done = arguments[0]
+                    fetch('https://schuhservice.schuh.co.uk/StoreFinderService/GetNearbyBranchesByLocation', {
+                "headers": {
+                    "accept": "application/json, text/javascript, */*; q=0.01",
+                    "accept-language": "en-US,en;q=0.9",
+                    "cache-control": "no-cache",
+                    "content-type": "application/json;charset=UTF-8;",
+                    "sec-ch-ua-mobile": "?0",
+                    "sec-fetch-dest": "empty",
+                    "sec-fetch-mode": "cors",
+                    "sec-fetch-site": "same-site"
+                },
+                "referrer": "https://www.schuh.co.uk/",
+                "referrerPolicy": "strict-origin-when-cross-origin",
+                "body": '{"lat":"""
+                    + str(search_lat)
+                    + r""","lon":"""
+                    + str(search_lon)
+                    + r""","culture":"en-gb"}',
+                "method": "POST",
+                "mode": "cors",
+                "credentials": "include"
+                    })
+                    .then(res => res.json())
+                    .then(data => done(data))
+                    """
+                )
+                break
             except Exception:
-                driver = get_driver(page_url, "inside-page-hero", driver=driver)
-
-        response = driver.page_source
-        soup = bs(response, "html.parser")
-
-        map_object = soup.find("div", attrs={"class": "wpgmza_map"})
-        if map_object is None:
-            if page_url == "https://headquartersoffice.com/privacy/" or page_url == "https://headquartersoffice.com/" or "?" in soup.find("h1").text.strip():
+                driver = get_driver(
+                    "https://www.schuh.co.uk/stores/", "secondLine", driver=driver
+                )
                 continue
-            
-            locator_domain = "headquartersoffice.com"
-            location_name = soup.find("h1").text.strip()
-            latitude = "<MISSING>"
-            longitude = "<MISSING>"
-            city = "<INACCESSIBLE"
-            store_number = "<MISSING>"
-            address = "<INACCESSIBLE>"
-            state = "<INACCESSIBLE>"
-            zipp = "<INACCESSIBLE>"
-            phone = "<INACCESSIBLE>"
-            location_type = "<INACCESSIBLE>"
-            hours = "<MISSING>"
-            country_code = "<MISSING>"
+
+        data = data["d"]
+        data = (
+            data.replace('"', "'")
+            .replace("true", "True")
+            .replace("false", "False")
+            .replace("'s", "s")
+        )
+
+        try:
+            data = ast.literal_eval(data)
+        except Exception:
+            continue
+
+        for location in data:
+            locator_domain = "schuh.co.uk"
+            page_url = "https://www.schuh.co.uk/stores/" + location[
+                "BranchName"
+            ].lower().replace(" ", "-")
+            location_name = location["BranchName"]
+            city = location["BranchCity"]
+            zipp = location["BranchPostcode"]
+            country_code = "UK"
+            store_number = location["BranchRef"]
+            phone = location["BranchPhone"]
+            latitude = location["BranchLatitude"]
+            longitude = location["BranchLongitude"]
+
+            while True:
+                try:
+                    location_data = driver.execute_async_script(
+                        r"""
+                        var done = arguments[0]
+                        fetch("https://schuhservice.schuh.co.uk/StoreFinderService/GetAdditionalBranchInfo", {
+                        "headers": {
+                            "accept": "application/json, text/javascript, */*; q=0.01",
+                            "accept-language": "en-US,en;q=0.9",
+                            "cache-control": "no-cache",
+                            "content-type": "application/json;charset=UTF-8;",
+                            "sec-ch-ua-mobile": "?0",
+                            "sec-fetch-dest": "empty",
+                            "sec-fetch-mode": "cors",
+                            "sec-fetch-site": "same-site"
+                        },
+                        "referrer": "https://www.schuh.co.uk/",
+                        "referrerPolicy": "strict-origin-when-cross-origin",
+                        "body": '{"branchName":\""""
+                        + location["BranchName"].lower().replace(" ", "-")
+                        + r"""\","culture":"en-gb"}',
+                        "method": "POST",
+                        "mode": "cors",
+                        "credentials": "include"
+                        })
+                        .then(res => res.json())
+                        .then(data => done(data))
+                        """
+                    )
+                    break
+                except Exception:
+                    driver = get_driver(
+                        "https://www.schuh.co.uk/stores/", "secondLine", driver=driver
+                    )
+                    continue
+
+            location_data = location_data["d"]
+
+            location_data = (
+                location_data.replace('"', "'")
+                .replace("true", "True")
+                .replace("false", "False")
+                .replace("\\", "")
+                .split(",'BranchLocalInfo'")[0]
+                .replace("'s", "s")
+                + "}"
+            )
+
+            location_data = ast.literal_eval(location_data)
+
+            address = (
+                location_data["BranchAddress1"] + " " + location_data["BranchAddress2"]
+            )
+
+            hours = ""
+            for open_key, close_key in hours_key_list:
+                day = open_key[:3]
+                open_time = (
+                    str(location_data[open_key])[:-2]
+                    + ":"
+                    + str(location_data[open_key])[-2:]
+                )
+                end_time = (
+                    str(location_data[close_key])[:-2]
+                    + ":"
+                    + str(location_data[close_key])[-2:]
+                )
+
+                hours = hours + day + " " + open_time + "-" + end_time + ", "
+            hours = hours[:-2]
+
+            state = "<MISSING>"
+            location_type = "<MISSING>"
+
+            search.found_location_at(latitude, longitude)
 
             yield {
                 "locator_domain": locator_domain,
@@ -349,39 +226,6 @@ def get_data():
                 "hours": hours,
                 "country_code": country_code,
             }
-        
-        else:
-            time.sleep(1)
-            test = driver.execute_script("var performance = window.performance || window.mozPerformance || window.msPerformance || window.webkitPerformance || {}; var network = performance.getEntries() || {}; return network;")
-            
-            found = 0
-            for item in test:
-                if "base64" in item["name"] and "marker-list" in item["name"]:
-                    x = x+1
-                    locations = new_map_page(driver)
-                    found = 1
-                    if len(locations) == 0:
-                        print("")
-                        print("new map")
-                        print(driver.current_url)
-                    for loc in locations:
-                        yield loc
-
-                    break
-
-
-            if found == 0:
-                locations = old_map_page(driver)
-                if len(locations) == 0:
-                    print("")
-                    print("old map")
-                    print(driver.current_url)
-                for loc in locations:
-                    yield loc
-                y = y+1
-
-    print(x)
-    print(y)
 
 
 def scrape():
@@ -389,10 +233,14 @@ def scrape():
         locator_domain=sp.MappingField(mapping=["locator_domain"]),
         page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
         location_name=sp.MappingField(
-            mapping=["location_name"], part_of_record_identity=True
+            mapping=["location_name"],
         ),
-        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
-        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
+        latitude=sp.MappingField(
+            mapping=["latitude"],
+        ),
+        longitude=sp.MappingField(
+            mapping=["longitude"],
+        ),
         street_address=sp.MultiMappingField(
             mapping=["street_address"], is_required=False
         ),
