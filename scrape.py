47c86c70948082d3cd6c4ede5d1511565b366d02
extrasauceontop <msuchemba@gmail.com>
2022-03-09 13:48:33 -0600

    scrape

--- scrape.py
+++ scrape.py
@@ -112,7 +112,7 @@ def get_data():
         hours_text = hours_soup.text.strip()
 
 
-        print((hours_text.replace("\n", ", ").split(" Mall")[0].replace(" , ", ", ")[:-2]).strip())
+        print((hours_text.replace("\n", ", ").split(", Mall")[0].replace(" , ", ", ")).strip())
         print("")
         
         hours = "<LATER>"
