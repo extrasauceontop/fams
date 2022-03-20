9ffffb39dc5caa3bf378db7b753b81321fe94a67
extrasauceontop <msuchemba@gmail.com>
2022-03-20 16:50:03 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,8 +1,15 @@
 import camelot
+from sgrequests import SgRequests
 
-file = "my_pdf.pdf"
+session = SgRequests()
+pdf_url = "https://www.bbva.pe/content/dam/public-web/peru/documents/personas/canales-de-atencion/oficinas/Oficinas-BBVA-abiertas-23.10.20.pdf"
+pdf_response = session.get(pdf_url)
+my_raw_data = pdf_response.content
 
-tables = camelot.read_pdf(file)
+with open("my_pdf.pdf", "wb") as my_data:
+    my_data.write(my_raw_data)
+
+tables = camelot.read_pdf("my_pdf.pdf")
 print("Total tables extracted:", tables.n)
 
 print(tables[0].df)
