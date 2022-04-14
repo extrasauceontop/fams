081ca23e2b467cec231cb20cfcd6dce8939ab2b3
extrasauceontop <msuchemba@gmail.com>
2022-04-14 12:06:22 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,203 +1,155 @@
-from sgrequests import SgRequests
-from sglogging import SgLogSetup
-from sgscrape.sgwriter import SgWriter
-from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgselenium.sgselenium import SgChrome
+from webdriver_manager.chrome import ChromeDriverManager
+from sgzip.dynamic import DynamicZipSearch, SearchableCountries, Grain_8
+from sgscrape import simple_scraper_pipeline as sp
+import json
+from bs4 import BeautifulSoup as bs
+import ssl
 
-logger = SgLogSetup().get_logger("homedepot_com")
-
-session = SgRequests()
-headers = {
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36"
-}
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def fetch_data():
-    locs = []
-    states = []
-    url = "https://www.homedepot.com/l/storeDirectory"
-    r = session.get(url, headers=headers)
-    for line in r.iter_lines():
-        if '<a class="store-directory__state-link' in line:
-            items = line.split('<a class="store-directory__state-link')
-            for item in items:
-                if 'href="https://www.homedepot.com/l/' in item:
-                    lurl = item.split('href="')[1].split('"')[0]
-                    if lurl not in states:
-                        states.append(lurl)
-    for state in states:
-        Found = True
-        while Found:
-            logger.info(state)
-            r2 = session.get(state, headers=headers)
-            for line2 in r2.iter_lines():
-                if '","url":"https://www.homedepot.com/' in line2:
-                    items = line2.split('","url":"https://www.homedepot.com/')
-                    for item in items:
-                        if '","phone":' in item:
-                            Found = False
-                            surl = "https://www.homedepot.com/" + item.split('"')[0]
-                            if surl not in locs:
-                                locs.append(surl)
+def get_driver(url, driver=None):
+    if driver is not None:
+        driver.quit()
 
-    logger.info(len(locs))
+    user_agent = (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+    )
+    x = 0
+    while True:
+        x = x + 1
+        try:
+            driver = SgChrome(
+                executable_path=ChromeDriverManager().install(),
+                user_agent=user_agent,
+                is_headless=True,
+            ).driver()
+            driver.get(url)
 
-    for loc in locs:
-        if "/designcenter" in loc:
-            continue
-        if loc == "https://www.homedepot.com/l/storeDirectory":
-            continue
-
-        Found = True
-        rcount = 0
-        while Found and rcount <= 3:
-            try:
-                rcount = rcount + 1
-                logger.info("Pulling Location %s..." % loc)
-                website = "homedepot.com"
-                typ = "<MISSING>"
-                hours = ""
-                name = ""
-                add = ""
-                city = ""
-                state = ""
-                zc = ""
-                country = "US"
-                store = ""
-                phone = ""
-                lat = ""
-                lng = ""
-                phone = ""
-                r2 = session.get(loc, headers=headers)
-                for line2 in r2.iter_lines():
-                    if "<h1" in line2:
-                        name = line2.split("<h1")[1].split('">')[1].split("<")[0]
-                    if '"stores":[{' in line2:
-                        Found = False
-                        add = line2.split('"street":"')[1].split('"')[0]
-                        city = line2.split('"city":"')[1].split('"')[0]
-                        state = line2.split('"state":"')[1].split('"')[0]
-                        zc = line2.split('"postalCode":"')[1].split('"')[0]
-                        store = line2.split('"storeId":"')[1].split('"')[0]
-                        lng = line2.split('"lng":')[1].split("}")[0]
-                        lat = line2.split('"lat":')[1].split(",")[0]
-                    if '"openingHours":["' in line2:
-                        hours = (
-                            line2.split('"openingHours":["')[1]
-                            .split('"],')[0]
-                            .replace('","', "; ")
-                            .replace('"', "")
-                        )
-                    if phone == "" and 'href="tel:' in line2:
-                        phone = line2.split('href="tel:')[1].split('"')[0]
-                if hours == "":
-                    hours = "<MISSING>"
-                if state == "PR":
-                    country = "Puerto Rico"
-                    state = "<MISSING>"
-                if state == "VI":
-                    country = "US Virgin Islands"
-                    state = "<MISSING>"
-                if state == "GU":
-                    country = "Guam"
-                    state = "<MISSING>"
-                
-                if name == "" or name == "<MISSING>":
-                    print(r2.text)
-                    raise Exception
-
-                yield SgRecord(
-                    locator_domain=website,
-                    page_url=loc,
-                    location_name=name,
-                    street_address=add,
-                    city=city,
-                    state=state,
-                    zip_postal=zc,
-                    country_code=country,
-                    phone=phone,
-                    location_type=typ,
-                    store_number=store,
-                    latitude=lat,
-                    longitude=lng,
-                    hours_of_operation=hours,
+            break
+        except Exception:
+            driver.quit()
+            if x == 10:
+                raise Exception(
+                    "Make sure this ran with a Proxy, will fail without one"
                 )
+            continue
+
+    return driver
+
+
+def get_data():
+    search = DynamicZipSearch(
+        country_codes=[SearchableCountries.USA], granularity=Grain_8()
+    )
+
+    start = 0
+    for zip_code in search:
+        data_url = (
+            "https://prismahealth.org/api/search/search?pageSize=100&pageNumber=1&searchType=location&zip="
+            + zip_code
+            + "&radius=1000"
+        )
+
+        if start == 0:
+
+            driver = get_driver(data_url)
+            source = driver.page_source
+            soup = bs(source, "html.parser")
+            text = soup.text.strip()
+            response_json = json.loads(text)
+            start = 1
+
+        else:
+            try:
+                driver.get(data_url)
+                source = driver.page_source
+                soup = bs(source, "html.parser")
+                text = soup.text.strip()
+                response_json = json.loads(text)
 
             except Exception:
-                if rcount == 3:
-                    raise Exception
+                driver = get_driver(data_url)
+                source = driver.page_source
+                soup = bs(source, "html.parser")
+                text = soup.text.strip()
+                response_json = json.loads(text)
 
-                else:
-                    pass
+        locations = response_json["Items"]
 
-    website = "homedepot.com"
-    typ = "<MISSING>"
-    hours = "Mon-Sat 9:00am - 7:00pm; Sun 10:00am - 5:00pm"
-    name = "San Diego"
-    add = "9480 Carroll Park Drive"
-    city = "San Diego"
-    state = "CA"
-    zc = "92121"
-    country = "US"
-    store = "<MISSING>"
-    phone = "858-812-8680"
-    lat = "32.8865868"
-    lng = "-117.1739095"
-    loc = "https://hddc-appointment.extapps.homedepot.com/storeSelection"
-    yield SgRecord(
-        locator_domain=website,
-        page_url=loc,
-        location_name=name,
-        street_address=add,
-        city=city,
-        state=state,
-        zip_postal=zc,
-        country_code=country,
-        phone=phone,
-        location_type=typ,
-        store_number=store,
-        latitude=lat,
-        longitude=lng,
-        hours_of_operation=hours,
-    )
-    website = "homedepot.com"
-    typ = "<MISSING>"
-    hours = "Mon-Sat 10:00am - 7:00pm; Sun 10:00am - 6:00pm"
-    name = "Rockville"
-    add = "12087 Rockville Pike"
-    city = "Rockville"
-    state = "MD"
-    zc = "20852"
-    country = "US"
-    store = "<MISSING>"
-    phone = "301-692-3700"
-    lat = "39.0563569"
-    lng = "-77.1188593"
-    loc = "https://www.homedepot.com/c/designcenter"
-    yield SgRecord(
-        locator_domain=website,
-        page_url=loc,
-        location_name=name,
-        street_address=add,
-        city=city,
-        state=state,
-        zip_postal=zc,
-        country_code=country,
-        phone=phone,
-        location_type=typ,
-        store_number=store,
-        latitude=lat,
-        longitude=lng,
-        hours_of_operation=hours,
-    )
+        for location in locations:
+            locator_domain = "prismahealth.org"
+            page_url = data_url
+            location_name = location["Title"]
+
+            address = location["Address"][0] + " " + location["Address"][1]
+
+            city = location["Address"][2].split(",")[0]
+            state = location["Address"][2].split(" ")[-2]
+            zipp = location["Address"][2].split(" ")[-1]
+
+            country_code = "US"
+            store_number = location["Id"].split(";")[0]
+
+            phone = location["Phone"]
+            location_type = "<MISSING>"
+            latitude = location["Coordinate"]["Latitude"]
+            longitude = location["Coordinate"]["Longitude"]
+            search.found_location_at(latitude, longitude)
+            hours = "<MISSING>"
+
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "store_number": store_number,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
 
 
 def scrape():
-    results = fetch_data()
-    with SgWriter(deduper=SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
-        for rec in results:
-            writer.write_row(rec)
+    field_defs = sp.SimpleScraperPipeline.field_definitions(
+        locator_domain=sp.MappingField(mapping=["locator_domain"]),
+        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
+        location_name=sp.MappingField(
+            mapping=["location_name"], part_of_record_identity=True
+        ),
+        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
+        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
+        street_address=sp.MultiMappingField(
+            mapping=["street_address"], is_required=False
+        ),
+        city=sp.MappingField(
+            mapping=["city"],
+        ),
+        state=sp.MappingField(mapping=["state"], is_required=False),
+        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
+        country_code=sp.MappingField(mapping=["country_code"]),
+        phone=sp.MappingField(mapping=["phone"], is_required=False),
+        store_number=sp.MappingField(
+            mapping=["store_number"], part_of_record_identity=True
+        ),
+        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
+        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
+    )
+
+    pipeline = sp.SimpleScraperPipeline(
+        scraper_name="Crawler",
+        data_fetcher=get_data,
+        field_definitions=field_defs,
+        log_stats_interval=15,
+    )
+    pipeline.run()
 
 
 scrape()
