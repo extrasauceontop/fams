ce38c60186f2b2039f8f6714dd3b45400b1e1f46
extrasauceontop <msuchemba@gmail.com>
2022-04-21 13:13:24 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 ssl._create_default_https_context = ssl._create_unverified_context
 
 with SgChrome(
-    executable_path=ChromeDriverManager().install(),
+    # executable_path=ChromeDriverManager().install(),
     user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
     is_headless=True,
 ) as driver:
