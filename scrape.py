b28a5f80fb81cb4e172af643910e44a5a5450571
extrasauceontop <msuchemba@gmail.com>
2022-04-27 15:26:28 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,115 +1,143 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from webdriver_manager.chrome import ChromeDriverManager
-from bs4 import BeautifulSoup as bs
-from sgscrape import simple_scraper_pipeline as sp
-from selenium import webdriver
-import undetected_chromedriver as uc
+from sglogging import sglog
+from bs4 import BeautifulSoup
+from sgrequests import SgRequests
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgselenium import SgChrome
 import ssl
+import json
 
 ssl._create_default_https_context = ssl._create_unverified_context
+session = SgRequests()
+website = "bathandbodyworks_com"
+log = sglog.SgLogSetup().get_logger(logger_name=website)
+
+headers = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36"
+}
+
+DOMAIN = "https://www.bathandbodyworks.com"
+MISSING = SgRecord.MISSING
 
 
-def get_driver(url, class_name, driver=None):
+def extract_json(html_string):
+    json_objects = []
+    count = 0
 
-    options = webdriver.ChromeOptions()
-    options.add_argument("start-maximized")
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    driver = uc.Chrome(
-        executable_path=ChromeDriverManager().install(), options=options
+    brace_count = 0
+    for element in html_string:
+
+        if element == "{":
+            brace_count = brace_count + 1
+            if brace_count == 1:
+                start = count
+
+        elif element == "}":
+            brace_count = brace_count - 1
+            if brace_count == 0:
+                end = count
+                try:
+                    json_objects.append(json.loads(html_string[start : end + 1]))
+                except Exception:
+                    pass
+        count = count + 1
+
+    return json_objects
+
+
+def fetch_data():
+    base_url = (
+        "https://www.bathandbodyworks.com/north-america/global-locations-canada.html"
     )
+    r = session.get(base_url, headers=headers)
+    soup = BeautifulSoup(r.text, "lxml")
+    data = soup.find_all("div", {"class": "store-location"})
+    for i in data:
+        data2 = i.find("p", {"class": "location"})
+        state = data2.text.split(",")[-1].strip()
+        city = data2.text.split(",")[0].strip()
+        location_name = i.find("p").text
+        data3 = i.find("p", {"class": "title"})
+        phone = data3.find_next_sibling().text
+        log.info(phone)
+        yield SgRecord(
+            locator_domain=DOMAIN,
+            page_url=base_url,
+            location_name=location_name,
+            street_address=MISSING,
+            city=city,
+            state=state,
+            zip_postal=MISSING,
+            country_code="CA",
+            store_number=MISSING,
+            phone=phone.strip(),
+            location_type=MISSING,
+            latitude=MISSING,
+            longitude=MISSING,
+            hours_of_operation=MISSING,
+        )
+    base_url = "https://www.bathandbodyworks.com"
 
-    driver.get(url)
 
-    WebDriverWait(driver, 20).until(
-        EC.presence_of_element_located((By.CLASS_NAME, class_name))
-    )
-    return driver
-
-
-def get_data():
-    url = "https://baddaddysburgerbar.com/find-us"
-    class_name = "text-color-primary"
-    with get_driver(url, class_name) as driver:
+    url = "https://www.bathandbodyworks.com/on/demandware.store/Sites-BathAndBodyWorks-Site/en_US/Stores-GetNearestStores?latitude=40.7895453&longitude=-74.05652980000002&countryCode=US&distanceUnit=mi&maxdistance=100000&BBW=1"
+    with SgChrome(is_headless=False).driver() as driver:
+        driver.get(url)
         response = driver.page_source
     
-    soup = bs(response, "html.parser")
-    grids = soup.find_all("div", attrs={"class": "h-fit-content"})
-    for grid in grids:
-        locator_domain = "baddaddysburgerbar.com"
-        page_url = grid.find_all("a")[-1]["href"]
-        location_name = grid.find("div").find("div").text.strip()
-        address = grid.find("div").find_all("div")[1].text.strip()
-        city = grid.find("div").find_all("div")[2].text.strip().split(", ")[0]
-        state = grid.find("div").find_all("div")[2].text.strip().split(", ")[1].split(" ")[0]
-        zipp = grid.find("div").find_all("div")[2].text.strip().split(", ")[1].split(" ")[1]
-        phone = grid.find("div", attrs={"class": "pb-3"}).find("a")["href"].replace("tel:", "")
-        location_type = "<MISSING>"
-        country_code = "US"
-        latitude = "<MISSING>"
-        longitude = "<MISSING>"
-        store_number = "<MISSING>"
+    json_objects = extract_json(response)
 
-        hours_parts = grid.find("div", attrs={"class": "pb-3"}).text.strip().replace("\n", "")
-        while "  " in hours_parts:
-            hours_parts = hours_parts.replace("  ", " ")
-
-        hours = hours_parts.split(phone[-3:])[1]
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "store_number": store_number,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        }
-
+    location_data = json_objects[0]["stores"]
+    for key in location_data:
+        store_data = location_data[key]
+        location_name = store_data["name"]
+        street_address = store_data["address1"] + " " + store_data["address2"]
+        city = store_data["city"]
+        state = store_data["stateCode"]
+        zip_postal = store_data["postalCode"]
+        country_code = store_data["countryCode"]
+        phone = store_data["phone"]
+        log.info(phone)
+        store_number = key
+        latitude = store_data["latitude"]
+        longitude = store_data["longitude"]
+        hours_of_operation = " ".join(
+            list(BeautifulSoup(store_data["storeHours"], "lxml").stripped_strings)
+        )
+        url = "https://www.bathandbodyworks.com/store-locator"
+        yield SgRecord(
+            locator_domain=DOMAIN,
+            page_url=url,
+            location_name=location_name,
+            street_address=street_address.strip(),
+            city=city.strip(),
+            state=state.strip(),
+            zip_postal=zip_postal.strip(),
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone.strip(),
+            location_type=MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+        )
 
 
 def scrape():
-    field_defs = sp.SimpleScraperPipeline.field_definitions(
-        locator_domain=sp.MappingField(mapping=["locator_domain"]),
-        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
-        location_name=sp.MappingField(
-            mapping=["location_name"], part_of_record_identity=True
-        ),
-        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
-        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
-        street_address=sp.MultiMappingField(
-            mapping=["street_address"], is_required=False
-        ),
-        city=sp.MappingField(
-            mapping=["city"],
-        ),
-        state=sp.MappingField(mapping=["state"], is_required=False),
-        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
-        country_code=sp.MappingField(mapping=["country_code"]),
-        phone=sp.MappingField(mapping=["phone"], is_required=False),
-        store_number=sp.MappingField(
-            mapping=["store_number"], part_of_record_identity=True
-        ),
-        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
-        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
-    )
+    log.info("Started")
+    count = 0
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PhoneNumberId)
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
 
-    pipeline = sp.SimpleScraperPipeline(
-        scraper_name="Crawler",
-        data_fetcher=get_data,
-        field_definitions=field_defs,
-        log_stats_interval=15,
-    )
-    pipeline.run()
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
 
 
-scrape()
+if __name__ == "__main__":
+    scrape()
