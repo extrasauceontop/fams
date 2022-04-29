dfb55fc6c050075911b15eece8ed52045393e599
extrasauceontop <msuchemba@gmail.com>
2022-04-29 13:55:43 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,5 +1,8 @@
 from sgselenium import SgFirefox
 import time
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 with SgFirefox(block_third_parties=True, is_headless=True) as driver:
     url = "https://baddaddysburgerbar.com/find-us"
