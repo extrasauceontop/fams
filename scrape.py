295af0381d1fa00c0707239d65634b3aebac12de
extrasauceontop <msuchemba@gmail.com>
2022-04-29 10:49:21 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 from sgselenium import SgFirefox
 import time
 
-with SgFirefox(block_third_parties=True, is_headless=False) as driver:
+with SgFirefox(block_third_parties=True, is_headless=True) as driver:
     url = "https://baddaddysburgerbar.com/find-us"
     driver.get(url)
     time.sleep(10)
