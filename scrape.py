ed8a5ec74be9bf8394b081ed2ff8e29cd961413d
extrasauceontop <msuchemba@gmail.com>
2022-04-29 13:49:11 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -5,6 +5,5 @@ with SgFirefox(block_third_parties=True, is_headless=True) as driver:
     url = "https://baddaddysburgerbar.com/find-us"
     driver.get(url)
     time.sleep(10)
-
     response = driver.page_source
     print(response)
