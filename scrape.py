d6f8d94e59b918234f8411661fb4625897b13c9a
extrasauceontop <msuchemba@gmail.com>
2022-05-05 16:27:03 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,33 +1,77 @@
-from sgselenium import SgChrome
-from webdriver_manager.chrome import ChromeDriverManager
-import time
+# -*- coding: utf-8 -*-
+from lxml import etree
+
+from sgrequests import SgRequests
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgwriter import SgWriter
+from sgselenium.sgselenium import SgFirefox
 
 
-user_agent = (
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
-)
-url = "https://order.wingzone.com/"
+def fetch_data():
+    session = SgRequests(verify_ssl=False)
+    start_url = "https://www.orange.be/nl/shop_locator/shop.json?LCZSG"
+    domain = "orange.be"
+    hdr = {
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+    }
 
-with SgChrome(executable_path=ChromeDriverManager().install(), user_agent=user_agent, is_headless=True, extra_option_args=["start-maximized"]).driver() as driver:
-    driver.get(url)
-    time.sleep(2)
-    driver.find_elements_by_class_name("styles__StyledPrimaryButton-sc-3mz1a9-0")[1].click()
-    time.sleep(2)
-    response = driver.page_source
+    all_locations = session.get(start_url, headers=hdr).json()
+    with SgFirefox() as driver:
+        for poi in all_locations:
+            page_url = f'https://www.orange.be/nl/shops/{poi["slug"]}'
+            poi_data = session.get(
+                f'https://www.orange.be/nl/shop_locator/shop/{poi["slug"]}.json'
+            ).json()
+            driver.get(page_url)
+            loc_dom = etree.HTML(driver.page_source)
+            hoo = loc_dom.xpath('//div[@class="hours-week"]//text()')
+            hoo = " ".join([e.strip() for e in hoo if e.strip()])
+            phone = poi_data["phone"]
+            if phone and phone == "./..":
+                phone = ""
+            street_address = poi["address"]["thoroughfare"]
+            if street_address == "-":
+                street_address = ""
+            zip_code = poi["address"]["postal_code"]
+            if zip_code == "-":
+                zip_code = ""
+            city = poi["address"]["locality"]
+            if city == "-":
+                city = ""
 
-    data = driver.execute_async_script(
-        """
-        var done = arguments[0]
-        fetch("https://api.koala.io/v1/ordering/store-locations/?sort[state_id]=asc&sort[label]=asc&include[]=operating_hours&include[]=attributes&include[]=delivery_hours&paginate=false", {
-            "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": null,
-            "method": "GET",
-            "mode": "cors",
-            "credentials": "omit"
-        })
-        .then(res => res.json())
-        .then(data => done(data))
-        """
-    )
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=page_url,
+                location_name=poi["name"],
+                street_address=street_address,
+                city=city,
+                state=poi["address"]["region"],
+                zip_postal=zip_code,
+                country_code=poi["address"]["country"],
+                store_number=poi["id"],
+                phone=phone,
+                location_type=poi["type"],
+                latitude=poi["lat"],
+                longitude=poi["lng"],
+                hours_of_operation=hoo,
+            )
 
-print(data)
+            yield item
+
+
+def scrape():
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
+            )
+        )
+    ) as writer:
+        for item in fetch_data():
+            writer.write_row(item)
+
+
+if __name__ == "__main__":
+    scrape()
