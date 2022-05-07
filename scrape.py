7268316c0ed720e763dc8d0570ba0e99a0005bd0
extrasauceontop <msuchemba@gmail.com>
2022-05-07 15:05:42 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 from lxml import etree
 
 from sgrequests import SgRequests
@@ -6,59 +5,73 @@ from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
-from sgselenium.sgselenium import SgFirefox
+# from sgrequests import ProxySettings
 
 
 def fetch_data():
-    session = SgRequests(verify_ssl=False)
-    start_url = "https://www.orange.be/nl/shop_locator/shop.json?LCZSG"
-    domain = "orange.be"
+    # session = SgRequests(proxy_escalation_order=ProxySettings.TEST_PROXY_ESCALATION_ORDER)
+    session = SgRequests()
+    start_url = "https://burtonsgrill.com/locations/"
+    domain = "burtonsgrill.com"
     hdr = {
         "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
     }
+    response = session.get(start_url, headers=hdr)
+    dom = etree.HTML(response.text)
 
-    all_locations = session.get(start_url, headers=hdr).json()
-    with SgFirefox() as driver:
-        for poi in all_locations:
-            page_url = f'https://www.orange.be/nl/shops/{poi["slug"]}'
-            poi_data = session.get(
-                f'https://www.orange.be/nl/shop_locator/shop/{poi["slug"]}.json'
-            ).json()
-            driver.get(page_url)
-            loc_dom = etree.HTML(driver.page_source)
-            hoo = loc_dom.xpath('//div[@class="hours-week"]//text()')
-            hoo = " ".join([e.strip() for e in hoo if e.strip()])
-            phone = poi_data["phone"]
-            if phone and phone == "./..":
-                phone = ""
-            street_address = poi["address"]["thoroughfare"]
-            if street_address == "-":
-                street_address = ""
-            zip_code = poi["address"]["postal_code"]
-            if zip_code == "-":
-                zip_code = ""
-            city = poi["address"]["locality"]
-            if city == "-":
-                city = ""
+    all_locations = dom.xpath('//div[@class="locations-group"]//a/@href')
+    for page_url in all_locations:
+        loc_response = session.get(page_url)
+        loc_dom = etree.HTML(loc_response.text)
 
-            item = SgRecord(
-                locator_domain=domain,
-                page_url=page_url,
-                location_name=poi["name"],
-                street_address=street_address,
-                city=city,
-                state=poi["address"]["region"],
-                zip_postal=zip_code,
-                country_code=poi["address"]["country"],
-                store_number=poi["id"],
-                phone=phone,
-                location_type=poi["type"],
-                latitude=poi["lat"],
-                longitude=poi["lng"],
-                hours_of_operation=hoo,
-            )
+        location_name = loc_dom.xpath('//h1[@class="title h3"]/text()')
+        location_name = location_name[0] if location_name else "<MISSING>"
+        raw_address = loc_dom.xpath('//a[contains(@href, "maps")]/text()')
+        if not raw_address:
+            continue
+        raw_address = raw_address[0].split(", ")
+        street_address = raw_address[0]
+        city = raw_address[1]
+        state = raw_address[-1].split()[0]
+        zip_code = raw_address[-1].split()[-1]
+        country_code = "<MISSING>"
+        store_number = "<MISSING>"
+        phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')
+        phone = phone[0] if phone else "<MISSING>"
+        location_type = "<MISSING>"
+        geo = (
+            loc_dom.xpath('//a[contains(@href, "maps")]/@href')[0]
+            .split("/@")[-1]
+            .split(",")[:2]
+        )
+        latitude = geo[0]
+        longitude = geo[1]
+        hoo = loc_dom.xpath(
+            '//div[i[@class="icon-clock"]]/following-sibling::div[1]//text()'
+        )
+        hoo = [e.strip() for e in hoo if e.strip()]
+        hours_of_operation = " ".join(hoo) if hoo else ""
+        if hours_of_operation and "Coming Soon" in hours_of_operation:
+            continue
 
-            yield item
+        item = SgRecord(
+            locator_domain=domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_code,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=location_type,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+        )
+
+        yield item
 
 
 def scrape():
