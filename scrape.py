af421831b4df1481e78d7ef2fb310c3d4c3f9864
extrasauceontop <msuchemba@gmail.com>
2022-05-11 12:55:33 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,74 +1,88 @@
 from lxml import etree
 
-from sgrequests import SgRequests
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
-from sgpostal.sgpostal import parse_address_intl
+# from sgpostal.sgpostal import parse_address_intl
+from sgselenium.sgselenium import SgChrome
+from webdriver_manager.chrome import ChromeDriverManager
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
 def fetch_data():
-    session = SgRequests()
+    user_agent = (
+        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+    )
     start_url = "https://burtonsgrill.com/locations/"
     domain = "burtonsgrill.com"
-    hdr = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    }
-    response = session.get(start_url, headers=hdr)
-    dom = etree.HTML(response.text)
+    
+    with SgChrome(executable_path=ChromeDriverManager().install(), user_agent=user_agent, is_headless=True).driver() as driver:
+        driver.get(start_url)
 
-    all_locations = dom.xpath('//div[@class="locations-group"]//a/@href')
-    for page_url in all_locations:
-        loc_response = session.get(page_url)
-        loc_dom = etree.HTML(loc_response.text)
+        dom = etree.HTML(driver.page_source)
 
-        location_name = loc_dom.xpath('//h1[@class="title h3"]/text()')
-        location_name = location_name[0] if location_name else ""
-        raw_address = loc_dom.xpath('//a[contains(@href, "maps")]/text()')
-        if not raw_address:
-            all_locations += loc_dom.xpath('//div[@class="locations-group"]//a/@href')
-            continue
-        raw_address = raw_address[0].split(", ")
-        street_address = raw_address[0]
-        addr = parse_address_intl(" ".join(raw_address))
-        phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')
-        phone = phone[0] if phone else ""
-        geo = (
-            loc_dom.xpath('//a[contains(@href, "maps")]/@href')[0]
-            .split("/@")[-1]
-            .split(",")[:2]
-        )
-        latitude = geo[0]
-        longitude = geo[1]
-        hoo = loc_dom.xpath(
-            '//div[i[@class="icon-clock"]]/following-sibling::div[1]//text()'
-        )
-        hoo = [e.strip() for e in hoo if e.strip()]
-        hours_of_operation = " ".join(hoo) if hoo else ""
-        if hours_of_operation and "Coming Soon" in hours_of_operation:
-            continue
+        all_locations = dom.xpath('//div[@class="locations-group"]//a/@href')
+        for page_url in all_locations:
+            driver.get(page_url)
+            loc_dom = etree.HTML(driver.page_source)
 
-        item = SgRecord(
-            locator_domain=domain,
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=addr.city,
-            state=addr.state,
-            zip_postal=addr.postcode,
-            country_code="",
-            store_number="",
-            phone=phone,
-            location_type="",
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-            raw_address=" ".join(raw_address),
-        )
+            location_name = loc_dom.xpath('//h1[@class="title h3"]/text()')
+            location_name = location_name[0] if location_name else ""
+            raw_address = loc_dom.xpath('//a[contains(@href, "maps")]/text()')
+            if not raw_address:
+                all_locations += loc_dom.xpath('//div[@class="locations-group"]//a/@href')
+                continue
+            raw_address = raw_address[0].split(", ")
+            street_address = raw_address[0]
+            # addr = parse_address_intl(" ".join(raw_address))
+            phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')
+            phone = phone[0] if phone else ""
+            geo = (
+                loc_dom.xpath('//a[contains(@href, "maps")]/@href')[0]
+                .split("/@")[-1]
+                .split(",")[:2]
+            )
+            latitude = geo[0]
+            longitude = geo[1]
+            hoo = loc_dom.xpath(
+                '//div[i[@class="icon-clock"]]/following-sibling::div[1]//text()'
+            )
+            hoo = [e.strip() for e in hoo if e.strip()]
+            hours_of_operation = " ".join(hoo) if hoo else ""
+            if hours_of_operation and "Coming Soon" in hours_of_operation:
+                continue
 
-        yield item
+            
+            # city=addr.city,
+            # state=addr.state,
+            # zip_postal=addr.postcode
+
+            city = "<MISSING>"
+            state = "<MISSING"
+            zipp = "<MISSING>"
+
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=page_url,
+                location_name=location_name,
+                street_address=street_address,
+                city=city,
+                state=state,
+                zip_postal=zipp,
+                country_code="",
+                store_number="",
+                phone=phone,
+                location_type="",
+                latitude=latitude,
+                longitude=longitude,
+                hours_of_operation=hours_of_operation,
+                raw_address=" ".join(raw_address),
+            )
+
+            yield item
 
 
 def scrape():
