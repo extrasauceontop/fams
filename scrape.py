34dab4c805422dd9c14e19e24529f066413493b2
extrasauceontop <msuchemba@gmail.com>
2022-05-11 12:19:08 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,88 +1,71 @@
-# -*- coding: utf-8 -*-
-from time import sleep
 from lxml import etree
-from urllib.parse import urljoin
 
 from sgrequests import SgRequests
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
-# from sgpostal.sgpostal import parse_address_intl
-from sgselenium.sgselenium import SgFirefox
+from sgpostal.sgpostal import parse_address_intl
 
 
 def fetch_data():
     session = SgRequests()
-
-    start_url = "https://www.adecco.no/kontakt-oss/"
-    domain = "adecco.no"
+    start_url = "https://burtonsgrill.com/locations/"
+    domain = "burtonsgrill.com"
     hdr = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36"
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_2_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     }
     response = session.get(start_url, headers=hdr)
     dom = etree.HTML(response.text)
 
-    all_locations = dom.xpath('//div[@class="row text-left"]//a/@href')
-    for url in all_locations:
-        page_url = urljoin(start_url, url)
+    all_locations = dom.xpath('//div[@class="locations-group"]//a/@href')
+    for page_url in all_locations:
         loc_response = session.get(page_url)
         loc_dom = etree.HTML(loc_response.text)
 
-        location_name = loc_dom.xpath("//h1/text()")[0]
-        phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')[-1]
+        location_name = loc_dom.xpath('//h1[@class="title h3"]/text()')
+        location_name = location_name[0] if location_name else ""
+        raw_address = loc_dom.xpath('//a[contains(@href, "maps")]/text()')
+        if not raw_address:
+            all_locations += loc_dom.xpath('//div[@class="locations-group"]//a/@href')
+            continue
+        raw_address = raw_address[0].split(", ")
+        street_address = raw_address[0]
+        addr = parse_address_intl(" ".join(raw_address))
+        phone = loc_dom.xpath('//a[contains(@href, "tel")]/text()')
+        phone = phone[0] if phone else ""
         geo = (
-            loc_dom.xpath('//iframe[@id="mapFrame"]/@src')[0]
-            .split("!2d")[-1]
-            .split("!2m")[0]
-            .split("!3d")
+            loc_dom.xpath('//a[contains(@href, "maps")]/@href')[0]
+            .split("/@")[-1]
+            .split(",")[:2]
         )
+        latitude = geo[0]
+        longitude = geo[1]
         hoo = loc_dom.xpath(
-            '//p[contains(text(), "Åpningstider:")]/following-sibling::p//text()'
-        )[0].replace("g0", "g 0")
-
-        x = 0
-        while True:
-            x = x+1
-            if x == 10:
-                print(loc_dom)
-                raise Exception
-            try:
-                with SgFirefox() as driver:
-                    driver.get(page_url)
-                    sleep(5)
-                    driver.switch_to.frame(driver.find_element_by_id("mapFrame"))
-                    loc_dom = etree.HTML(driver.page_source)
-                raw_address = loc_dom.xpath('//div[@class="address"]/text()')[0]
-                break
-
-            except Exception:
-                continue
-        
-        # addr = parse_address_intl(raw_address)
-        # street_address = addr.street_address_1
-        # if addr.street_address_2:
-        #     street_address += " " + addr.street_address_2
-        street_address = "<LATER>"
-        city = "<LATER>"
-        zipp = "<LATER>"
+            '//div[i[@class="icon-clock"]]/following-sibling::div[1]//text()'
+        )
+        hoo = [e.strip() for e in hoo if e.strip()]
+        hours_of_operation = " ".join(hoo) if hoo else ""
+        if hours_of_operation and "Coming Soon" in hours_of_operation:
+            continue
 
         item = SgRecord(
             locator_domain=domain,
             page_url=page_url,
             location_name=location_name,
             street_address=street_address,
-            city=city,
-            state="",
-            zip_postal=zipp,
-            country_code="NO",
+            city=addr.city,
+            state=addr.state,
+            zip_postal=addr.postcode,
+            country_code="",
             store_number="",
             phone=phone,
             location_type="",
-            latitude=geo[1],
-            longitude=geo[0],
-            hours_of_operation=hoo,
-            raw_address=raw_address,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+            raw_address=" ".join(raw_address),
         )
 
         yield item
