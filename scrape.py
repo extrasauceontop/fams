baff84bff638c8c91f129bd98942b03d9ff5299d
extrasauceontop <msuchemba@gmail.com>
2022-05-19 13:08:03 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,9 +1,6 @@
-from sgselenium import SgChrome
-import ssl
 import json
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
+from sgscrape import simple_scraper_pipeline as sp
+from sgselenium import SgChrome
 
 def extract_json(html_string):
     json_objects = []
@@ -30,11 +27,89 @@ def extract_json(html_string):
     return json_objects
 
 
-with SgChrome() as driver:
-    driver.get("https://www.zorbaz.com/locations")
-    driver.save_screenshot("zorbas.png")
-    response = driver.page_source
+def get_data():
+    with SgChrome as driver:
+        driver.get("https://www.zorbaz.com/locationz")
+        response = driver.page_source
 
-json_objects = extract_json(response.split("APOLLO_STATE")[1])[0]
+    json_objects = extract_json(response.split("APOLLO_STATE")[1])[0]
 
-print(json_objects)
+    for key in json_objects.keys():
+        if "RestaurantLocation:" in key:
+            location = json_objects[key]
+
+            locator_domain = "zorbaz.com"
+            page_url = "https://www.zorbaz.com/locationz"
+            location_name = location["name"]
+            latitude = location["lat"]
+            longitude = location["lng"]
+            city = location["city"]
+            store_number = key.split(":")[1]
+            address = location["streetAddress"]
+            state = location["state"]
+            zipp = location["postalCode"]
+            phone = location["phone"]
+            location_type = "<MISSING>"
+            country_code = "US"
+            
+            hours_parts = location["schemaHours"]
+            hours = ""
+            for part in hours_parts:
+                hours = hours + part + ", "
+            
+            hours = hours[:-2]
+
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "store_number": store_number,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
+
+
+def scrape():
+    field_defs = sp.SimpleScraperPipeline.field_definitions(
+        locator_domain=sp.MappingField(mapping=["locator_domain"]),
+        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
+        location_name=sp.MappingField(
+            mapping=["location_name"], part_of_record_identity=True
+        ),
+        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
+        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
+        street_address=sp.MultiMappingField(
+            mapping=["street_address"], is_required=False
+        ),
+        city=sp.MappingField(
+            mapping=["city"],
+        ),
+        state=sp.MappingField(mapping=["state"], is_required=False),
+        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
+        country_code=sp.MappingField(mapping=["country_code"]),
+        phone=sp.MappingField(mapping=["phone"], is_required=False),
+        store_number=sp.MappingField(
+            mapping=["store_number"], part_of_record_identity=True
+        ),
+        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
+        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
+    )
+
+    pipeline = sp.SimpleScraperPipeline(
+        scraper_name="Crawler",
+        data_fetcher=get_data,
+        field_definitions=field_defs,
+        log_stats_interval=15,
+    )
+    pipeline.run()
+
+
+scrape()
