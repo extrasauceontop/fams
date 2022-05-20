0583af5f03a727581b7f602fb81505c9f3c7d1c2
extrasauceontop <msuchemba@gmail.com>
2022-05-20 11:00:07 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,86 +1,119 @@
-from lxml import etree
-from sgrequests import SgRequests
-from sgselenium import SgFirefox
-from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
-from sgscrape.sgwriter import SgWriter
+import json
+from sgscrape import simple_scraper_pipeline as sp
+from sgselenium import SgChrome
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def fetch_data():
-    with SgFirefox() as driver:
-        start_url = "https://rocketfizz.com/?sl_engine=sl-xml"
-        driver.get(start_url)
+def extract_json(html_string):
+    json_objects = []
+    count = 0
+
+    brace_count = 0
+    for element in html_string:
+
+        if element == "{":
+            brace_count = brace_count + 1
+            if brace_count == 1:
+                start = count
+
+        elif element == "}":
+            brace_count = brace_count - 1
+            if brace_count == 0:
+                end = count
+                try:
+                    json_objects.append(json.loads(html_string[start : end + 1]))
+                except Exception:
+                    pass
+        count = count + 1
+
+    return json_objects
+
+
+def get_data():
+    with SgChrome() as driver:
+        driver.get("https://www.zorbaz.com/locationz")
         response = driver.page_source
 
-    domain = "rocketfizz.com"
-    session = SgRequests()
-    # print(response)
-    dom = etree.XML(response)
-    all_locations = dom.xpath("//marker")
-    print(len(all_locations))
-    x = 0
-    for poi_html in all_locations:
-        x = x+1
-        # if x == 10:
-        #     return
-        page_url = poi_html.xpath("@url")[0]
-        location_name = poi_html.xpath("@name")[0].replace("&#44;", ",")
-        street_address = poi_html.xpath("@street")[0].replace("&#44;", ",")
-        city = poi_html.xpath("@city")[0]
-        state = poi_html.xpath("@state")[0]
-        zip_code = poi_html.xpath("@zip")[0]
-        phone = poi_html.xpath("@phone")[0].replace("(FIZZ)", "")
-        phone = phone.replace("?", "") if phone else ""
-        latitude = poi_html.xpath("@lat")[0]
-        longitude = poi_html.xpath("@lng")[0]
+    json_objects = extract_json(response.split("APOLLO_STATE")[1])[0]
 
-        loc_response = session.get(page_url)
-        loc_dom = etree.HTML(loc_response.text)
-        days = loc_dom.xpath("//table/tbody/tr/td[1]/text()")[1:]
-        if not days:
-            days = loc_dom.xpath('//td[contains(text(), "Hours:")]/text()')[1:]
-        days = [elem.strip() for elem in days]
-        hours = loc_dom.xpath("//table/tbody/tr/td[2]/text()")[1:]
-        if not hours:
-            hours = loc_dom.xpath(
-                '//td[contains(text(), "Hours:")]/following-sibling::td/text()'
-            )[1:]
-        hours = [elem.strip() for elem in hours]
-        hours_of_operation = list(map(lambda day, hour: day + " " + hour, days, hours))
-        hours_of_operation = " ".join(hours_of_operation) if hours_of_operation else ""
+    for key in json_objects.keys():
+        if "RestaurantLocation:" in key:
+            location = json_objects[key]
 
-        item = SgRecord(
-            locator_domain=domain,
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=zip_code,
-            country_code="",
-            store_number="",
-            phone=phone,
-            location_type="",
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-        )
+            locator_domain = "zorbaz.com"
+            page_url = "https://www.zorbaz.com/locationz"
+            location_name = location["name"]
+            latitude = location["lat"]
+            longitude = location["lng"]
+            city = location["city"]
+            store_number = key.split(":")[1]
+            address = location["streetAddress"]
+            state = location["state"]
+            zipp = location["postalCode"]
+            phone = location["phone"]
+            location_type = "<MISSING>"
+            country_code = "US"
 
-        yield item
+            hours_parts = location["schemaHours"]
+            hours = ""
+            for part in hours_parts:
+                hours = hours + part + ", "
+
+            hours = hours[:-2]
+
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "store_number": store_number,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
 
 
 def scrape():
-    with SgWriter(
-        SgRecordDeduper(
-            SgRecordID(
-                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
-            )
-        )
-    ) as writer:
-        for item in fetch_data():
-            writer.write_row(item)
+    field_defs = sp.SimpleScraperPipeline.field_definitions(
+        locator_domain=sp.MappingField(mapping=["locator_domain"]),
+        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
+        location_name=sp.MappingField(
+            mapping=["location_name"], part_of_record_identity=True
+        ),
+        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
+        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
+        street_address=sp.MultiMappingField(
+            mapping=["street_address"], is_required=False
+        ),
+        city=sp.MappingField(
+            mapping=["city"],
+        ),
+        state=sp.MappingField(mapping=["state"], is_required=False),
+        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
+        country_code=sp.MappingField(mapping=["country_code"]),
+        phone=sp.MappingField(mapping=["phone"], is_required=False),
+        store_number=sp.MappingField(
+            mapping=["store_number"], part_of_record_identity=True
+        ),
+        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
+        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
+    )
+
+    pipeline = sp.SimpleScraperPipeline(
+        scraper_name="Crawler",
+        data_fetcher=get_data,
+        field_definitions=field_defs,
+        log_stats_interval=15,
+    )
+    pipeline.run()
 
 
-if __name__ == "__main__":
-    scrape()
+scrape()
