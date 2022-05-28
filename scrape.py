e1bdf96e9ecf17cedcc777771f2972d599f01781
extrasauceontop <msuchemba@gmail.com>
2022-05-28 14:31:59 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,120 +1,86 @@
-from sgscrape.sgrecord import SgRecord
+import json
 from sgscrape.sgwriter import SgWriter
-from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sglogging import SgLogSetup
-from sgrequests import SgRequests
-from sgselenium import SgChrome
-from sgzip.dynamic import DynamicZipSearch, SearchableCountries, Grain_4
-from bs4 import BeautifulSoup as bs
-import dirtyjson as json
 import ssl
-from webdriver_manager.chrome import ChromeDriverManager
-from tenacity import retry, stop_after_attempt, wait_fixed
-from proxyfier import ProxyProviders
+from sgselenium import SgFirefox
+import time
 
-try:
-    _create_unverified_https_context = (
-        ssl._create_unverified_context
-    )  # Legacy Python that doesn't verify HTTPS certificates by default
-except AttributeError:
-    pass
-else:
-    ssl._create_default_https_context = _create_unverified_https_context  # Handle target environment that doesn't support HTTPS verification
-
-logger = SgLogSetup().get_logger("")
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-locator_domain = "https://www.bupa.co.uk"
-base_url = "https://www.bupa.co.uk/dental/dental-care/practices?loc={}"
-bs_url = "https://www.bupa.co.uk/BDC/SearchPractices"
-json_url = "https://www.bupa.co.uk/BDC/GoogleMapSearch"
-
-
-@retry(wait=wait_fixed(2), stop=stop_after_attempt(3))
-def get_url(url=None):
-    rr = None
-    with SgChrome(
-        executable_path=ChromeDriverManager().install(),
-        user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-        is_headless=True,
-        proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER
-    ) as driver:
+def fetch_data():
+    with SgFirefox(is_headless=True) as driver:
+        url = "https://example.com/"
         driver.get(url)
-        rr = driver.wait_for_request(json_url, timeout=30)
+        time.sleep(10)
+        statelist = [
+            "ab",
+            "BC",
+            "MB",
+            "NB",
+            "NL",
+            "NT",
+            "NS",
+            "NU",
+            "ON",
+            "PE",
+            "QC",
+            "SK",
+            "YT",
+        ]
+        for st in statelist:
+            url = (
+                "https://www.atriaretirement.ca/wp-content/themes/aslblanktheme/script-getstatelocations.php?state="
+                + st
+            )
 
-    return rr
+            driver.get(url)
+            loclist = driver.page_source.split('"communities":', 1)[1].split("]}<", 1)[0]
+            loclist = loclist + "]"
+            loclist = json.loads(loclist)
+            for loc in loclist:
 
+                store = loc["community_number"]
+                title = loc["name"]
+                street = loc["address_1"] + loc["address_2"]
+                city = loc["city"]
+                state = loc["province"]
+                pcode = loc["postal_code"]
+                phone = loc["phone"]
+                lat = loc["latitude"]
+                longt = loc["longitude"]
+                link = loc["url"]
 
-def fetch_records(search):
-    for zip in search:
-        with SgRequests(proxy_country="us") as http:
-            try:
-                rr = get_url(
-                    f'https://www.bupa.co.uk/dental/dental-care/practices?loc={zip.replace(" ", "%20")}',
+                yield SgRecord(
+                    locator_domain="https://www.atriaretirement.ca/",
+                    page_url=link,
+                    location_name=title,
+                    street_address=street.strip(),
+                    city=city.strip(),
+                    state=state.strip(),
+                    zip_postal=pcode.strip(),
+                    country_code="CA",
+                    store_number=str(store),
+                    phone=phone.strip(),
+                    location_type=SgRecord.MISSING,
+                    latitude=str(lat),
+                    longitude=str(longt),
+                    hours_of_operation=SgRecord.MISSING,
                 )
-            except:
-                continue
 
-            headers = {}
-            for key, val in rr.headers.items():
-                if key.lower() != "content-length":
-                    headers[key] = val
-            page = 1
-            payload = json.loads(rr.body)
-            while True:
-                payload["pageIndex"] = page
-                try:
-                    locations = bs(
-                        http.post(bs_url, headers=headers, json=payload).text, "lxml"
-                    ).select("div.centervalign-outerwrapper")
-                except:
-                    break
-                if locations:
-                    page += 1
-                else:
-                    break
-                locs = http.post(json_url, headers=headers, json=payload).json()
-                payload["dataCount"] += len(locs)
-                logger.info(f"[{zip}] {len(locations)}")
-                for x, _ in enumerate(locs):
-                    page_url = locator_domain + _["PageUrl"]
-                    addr = _["FullAddress"].split(",")
-                    info = locations[x]
-                    hours = [
-                        ": ".join(hh.stripped_strings)
-                        for hh in info.select(
-                            "div.opening-hours-container table tbody tr"
-                        )
-                    ]
-                    phone = ""
-                    if info.select_one("a.tel-number"):
-                        phone = info.select_one("a.tel-number").text.strip()
-                    yield SgRecord(
-                        page_url=page_url,
-                        location_name=_["PageTitle"],
-                        street_address=", ".join(addr[:-2]),
-                        city=addr[-2],
-                        zip_postal=_["PostalCode"],
-                        country_code="uk",
-                        phone=phone,
-                        latitude=_["Latitude"],
-                        longitude=_["Longitude"],
-                        locator_domain=locator_domain,
-                        hours_of_operation="; ".join(hours),
-                        raw_address=_["FullAddress"],
-                    )
+
+def scrape():
+
+    with SgWriter(
+        deduper=SgRecordDeduper(record_id=RecommendedRecordIds.PageUrlId)
+    ) as writer:
+
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
 
 
 if __name__ == "__main__":
-    search = DynamicZipSearch(
-        country_codes=[SearchableCountries.BRITAIN], granularity=Grain_4(), expected_search_radius_miles=30
-    )
-    with SgWriter(
-        SgRecordDeduper(
-            SgRecordID({SgRecord.Headers.RAW_ADDRESS}),
-            duplicate_streak_failure_factor=100,
-        )
-    ) as writer:
-        for rec in fetch_records(search):
-            writer.write_row(rec)
+    scrape()
