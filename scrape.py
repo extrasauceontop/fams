b2b2b356f62a5d8437dc7cbcd532e407cf40d5f6
extrasauceontop <msuchemba@gmail.com>
2022-06-02 13:06:08 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -4,13 +4,13 @@ import ssl
 
 ssl._create_default_https_context = ssl._create_unverified_context
 
-if __name__ == "__main__":
-    user_agent = (
-        "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
-    )
-    url = "https://www.luckysupermarkets.com/stores/?coordinates=39.64096403685537,-112.39632159999998&zoom=5"
-    with SgChrome(user_agent=user_agent, is_headless=True, block_third_parties=False) as driver:
-        driver.get(url)
-        response = driver.page_source
+# if __name__ == "__main__":
+user_agent = (
+    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
+)
+url = "https://www.luckysupermarkets.com/stores/?coordinates=39.64096403685537,-112.39632159999998&zoom=5"
+with SgChrome() as driver:
+    driver.get(url)
+    response = driver.page_source
 
-    print(response)
+print(response)
