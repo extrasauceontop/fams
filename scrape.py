1e2df505dfba91a2ae2890ea0dcdc4011ec3c68b
extrasauceontop <msuchemba@gmail.com>
2022-06-02 13:12:31 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -9,7 +9,7 @@ user_agent = (
     "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
 )
 url = "https://www.luckysupermarkets.com/stores/?coordinates=39.64096403685537,-112.39632159999998&zoom=5"
-with SgChrome(proxy_provider_escalation_order=ProxyProviders.T) as driver:
+with SgChrome(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
     driver.get(url)
     response = driver.page_source
 
