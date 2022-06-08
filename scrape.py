0cfafc305103dc3b4c13b200d7cb71f99fda3933
extrasauceontop <msuchemba@gmail.com>
2022-06-08 11:58:04 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,90 +1,15 @@
-import re
-import json
-from lxml import etree
-
-from sgrequests import SgRequests
-from sgscrape.sgrecord import SgRecord
-from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sgscrape.sgrecord_id import SgRecordID
-from sgscrape.sgwriter import SgWriter
+from sgselenium import SgChrome
 from proxyfier import ProxyProviders
+import time
+import ssl
 
-
-def fetch_data():
-    session = SgRequests(proxy_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER)
-    domain = "dillards.com"
-    start_url = "https://www.dillards.com/stores"
-
-    headers = {
-        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.193 Safari/537.36",
-    }
-    response = session.get(start_url, headers=headers)
-    print(response)
-    dom = etree.HTML(response.text)
-
-    all_poi_urls = dom.xpath('//div[@id="storeListing"]//a[@class="underline"]/@href')
-    for url in all_poi_urls:
-        page_url = "https://www.dillards.com" + url
-        store_response = session.get(page_url)
-        store_dom = etree.HTML(store_response.text)
-        store_data = store_dom.xpath('//div[@id="storeDetails"]/script/text()')[0]
-        store_data = json.loads(store_data)
-
-        location_name = store_data["name"]
-        street_address = store_data["address"]["streetAddress"]
-        city = store_data["address"]["addressLocality"]
-        state = store_data["address"]["addressRegion"]
-        zip_code = store_data["address"]["postalCode"]
-        store_number = store_data["url"].split("/")[-1]
-        phone = store_data["telephone"]
-        location_type = store_data["@type"]
-
-        geo_data = store_dom.xpath(
-            '//script[contains(text(), "__INITIAL_STATE__")]/text()'
-        )[0]
-        geo_data = re.findall("__INITIAL_STATE__ =(.+);", geo_data)[0]
-        geo_data = json.loads(geo_data)
-        latitude = geo_data["contentData"]["store"]["latitude"]
-        longitude = geo_data["contentData"]["store"]["longitude"]
-        hoo = []
-        for elem in store_data["openingHoursSpecification"]:
-            day = elem["dayOfWeek"]["name"]
-            opens = elem["opens"]
-            closes = elem["closes"]
-            hoo.append(f"{day} {opens} - {closes}")
-        hours_of_operation = ", ".join(hoo) if hoo else ""
-
-        item = SgRecord(
-            locator_domain=domain,
-            page_url=page_url,
-            location_name=location_name,
-            street_address=street_address,
-            city=city,
-            state=state,
-            zip_postal=zip_code,
-            country_code="",
-            store_number=store_number,
-            phone=phone,
-            location_type=location_type,
-            latitude=latitude,
-            longitude=longitude,
-            hours_of_operation=hours_of_operation,
-        )
-
-        yield item
-
-
-def scrape():
-    with SgWriter(
-        SgRecordDeduper(
-            SgRecordID(
-                {SgRecord.Headers.LOCATION_NAME, SgRecord.Headers.STREET_ADDRESS}
-            )
-        )
-    ) as writer:
-        for item in fetch_data():
-            writer.write_row(item)
-
+ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
-    scrape()
+    with SgChrome(block_third_parties=False, is_headless=True, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, proxy_country="gr") as driver:
+        url = "https://mcdonalds.gr/%ce%b5%cf%83%cf%84%ce%b9%ce%b1%cf%84%cf%8c%cf%81%ce%b9%ce%b1/"
+        driver.get(url)
+        time.sleep(10)
+        response = driver.page_source
+        with open("file.txt", "w", encoding="utf-8") as output:
+            print(response, file=output)
