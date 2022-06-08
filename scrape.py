bca322ae6e014b2e54a5da9884dab06693aa30f0
extrasauceontop <msuchemba@gmail.com>
2022-06-08 12:06:00 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -6,7 +6,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
-    with SgChrome(block_third_parties=False, is_headless=True, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, proxy_country="gr") as driver:
+    with SgChrome(block_third_parties=True, is_headless=True, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, proxy_country="gr") as driver:
         url = "https://mcdonalds.gr/%ce%b5%cf%83%cf%84%ce%b9%ce%b1%cf%84%cf%8c%cf%81%ce%b9%ce%b1/"
         driver.get(url)
         time.sleep(10)
