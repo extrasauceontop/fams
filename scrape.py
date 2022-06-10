9c19f0dafcfb670fa6c445bef8c789fa914d83b1
extrasauceontop <msuchemba@gmail.com>
2022-06-10 13:08:57 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -304,7 +304,7 @@ def get_data():
         "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0"
     )
     class_name = "inside-page-hero"
-    with SgChrome(executable_path=ChromeDriverManager().install(), user_agent=user_agent, is_headless=True, block_third_parties=False) as driver:
+    with SgChrome(user_agent=user_agent, is_headless=True, block_third_parties=False) as driver:
         driver.maximize_window()
         for page_url in page_urls:
             count = 0
