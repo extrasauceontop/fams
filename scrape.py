2726f660dfc51ec535e6f5cd3ad0f6115232d9f8
extrasauceontop <msuchemba@gmail.com>
2022-06-14 16:07:13 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
 from sgselenium import SgFirefox
+from proxyfier import ProxyProviders
 
 url = "https://www.galerieslafayette.com/"
-with SgFirefox(is_headless=True, block_third_parties=False) as driver:
+with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
     driver.get(url)
     response = driver.page_source
     
