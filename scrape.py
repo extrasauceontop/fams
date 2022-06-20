c72c2214b4748e97793fb395c4f2e389def01f81
extrasauceontop <msuchemba@gmail.com>
2022-06-20 12:29:05 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,5 +1,8 @@
 from sgselenium import SgChrome
 from proxyfier import ProxyProviders
+import ssl
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 if __name__ == "__main__":
     url = "https://www.hibbett.com/on/demandware.store/Sites-Hibbett-US-Site/default/Stores-GetNearestStores?latitude=33.4306545&longitude=-86.7919009&countryCode=US&distanceUnit=mi&maxdistance=25&social=false"
