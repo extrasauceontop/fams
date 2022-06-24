6e3715756d13d0681b846b9e222abdb5520cf2b4
extrasauceontop <msuchemba@gmail.com>
2022-06-23 20:34:25 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,8 +1,14 @@
-from sgzip.dynamic import DynamicGeoSearch, SearchableCountries
+from sgselenium import SgFirefox
+from proxyfier import ProxyProviders
 
-search = DynamicGeoSearch(
-    country_codes=[SearchableCountries.USA]
-)
+url = "https://www.dominospizza.es/tiendas-dominos-pizza"
+proxy_url = "https://www.galerieslafayette.com/m/nos-magasins"
+with SgFirefox(proxy_country="es", proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
+    try:
+        driver.get(proxy_url)
+    except Exception: pass
+    driver.get(url)
+    response = driver.page_source
 
-for search_lat, search_lon in search:
-    print(search_lat, search_lon)
+
+print(response)
