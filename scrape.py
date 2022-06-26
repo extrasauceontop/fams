34520c8922ad94cc4e057a0937605a8bff928714
extrasauceontop <msuchemba@gmail.com>
2022-06-26 15:29:31 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ from proxyfier import ProxyProviders
 ssl._create_default_https_context = ssl._create_unverified_context
 
 url = "https://www.hibbett.com/on/demandware.store/Sites-Hibbett-US-Site/default/Stores-GetNearestStores?latitude=35.8409242&longitude=-78.62285039999999&countryCode=US&distanceUnit=mi&maxdistance=25&social=false"
-with SgFirefox(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, block_third_parties=False) as driver:
+with SgFirefox(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, block_third_parties=True) as driver:
     driver.get(url)
     response = driver.page_source
 
