f75861fc8c4f8eda35e6ec5f521ce06622549cda
extrasauceontop <msuchemba@gmail.com>
2022-06-28 15:02:52 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,131 +1,128 @@
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
+from sgselenium import SgChrome
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sglogging import SgLogSetup
+from datetime import datetime, timedelta
 from bs4 import BeautifulSoup as bs
-from sgscrape import simple_scraper_pipeline as sp
-import re
-from proxyfier import ProxyProviders
-import unidecode
-from sgpostal.sgpostal import parse_address_intl
+import math
+from concurrent.futures import ThreadPoolExecutor
+from webdriver_manager.chrome import ChromeDriverManager
+import os
+import ssl
 
-def check_response(response):
+ssl._create_default_https_context = ssl._create_unverified_context
+
+os.environ[
+    "PROXY_URL"
+] = "http://groups-RESIDENTIAL,country-us:{}@proxy.apify.com:8000/"
+
+logger = SgLogSetup().get_logger("quizclothing")
+_headers = {
+    "accept": "application/json",
+    "accept-language": "en-US,en;q=0.9",
+    "content-type": "application/json",
+    "origin": "https://mrbeastburger.com",
+    "referer": "https://mrbeastburger.com/",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
+}
+
+header2 = {
+    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.9",
+    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
+}
+
+locator_domain = "https://mrbeastburger.com"
+base_url = "https://api.dineengine.io/mrbeastburger/custom/dineengine/vendor/olo/restaurants?includePrivate=false"
+calendar_url = "https://api.dineengine.io/mrbeastburger/custom/dineengine/vendor/olo/restaurants/{}/calendars?from={}&to={}"
+today = datetime.today()
+mon = (today + timedelta(days=-today.weekday())).strftime("%Y%m%d")
+next_mon = (today + timedelta(days=-today.weekday(), weeks=1)).strftime("%Y%m%d")
+max_workers = 32
+
+
+def fetchConcurrentSingle(link):
+    page_url = locator_domain + "/locations" + link["url"].split("/menu")[-1]
+    logger.info(page_url)
     try:
-        if '<div id="mapa' not in response:
-            return False
-
-        else:
-            return True
-
-    except Exception:
-        return False
+        ca = request_with_retries(
+            calendar_url.format(link["id"], mon, next_mon)
+        ).json()["calendar"]
+    except:
+        ca = None
+    return link, page_url, ca
 
 
-def get_data():
-    url = "https://www.dominospizza.es/tiendas-dominos-pizza"
-    with SgRequests(
-        response_successful=check_response,
-        proxy_country="es",
-        proxy_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER
-    ) as session:
-        response = session.get(url).text
+def fetchConcurrentList(list, occurrence=max_workers):
+    output = []
+    total = len(list)
+    reminder = math.floor(total / 50)
+    if reminder < occurrence:
+        reminder = occurrence
 
-    soup = bs(response, "html.parser")
-    grids = soup.find("div", attrs={"id": "listaTiendas"}).find_all("ul")
+    count = 0
+    with ThreadPoolExecutor(
+        max_workers=occurrence, thread_name_prefix="fetcher"
+    ) as executor:
+        for result in executor.map(fetchConcurrentSingle, list):
+            if result:
+                count = count + 1
+                if count % reminder == 0:
+                    logger.debug(f"Concurrent Operation count = {count}")
+                output.append(result)
+    return output
 
-    for grid in grids:
-        zipp_index = -1
-        locator_domain = "www.dominospizza.es/"
-        page_url = (
-            "https://www.dominospizza.es"
-            + grid.find("a", attrs={"class": "nm"})["href"]
+
+def request_with_retries(url):
+    with SgRequests() as session:
+        return session.get(url, headers=_headers)
+
+
+def fetch_data():
+    with SgChrome(
+        executable_path=ChromeDriverManager().install(),
+        user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+        is_headless=True,
+    ) as driver:
+        driver.get(base_url)
+        locations = bs(driver.page_source, "lxml").select("restaurant")
+        for _, page_url, ca in fetchConcurrentList(locations):
+            hours = []
+            if ca:
+                for hr in ca["calendar"]:
+                    if not hr["label"]:
+                        for hh in hr["ranges"]:
+                            temp = f"{hh['weekday']}: {hh['start'].split()[-1]} - {hh['end'].split()[-1]}"
+                            if temp not in hours:
+                                hours.append(temp)
+                        break
+            yield SgRecord(
+                page_url=page_url,
+                store_number=_["id"],
+                location_name=_["name"],
+                street_address=_["streetaddress"],
+                city=_["city"],
+                state=_["state"],
+                zip_postal=_["zip"],
+                latitude=_["latitude"],
+                longitude=_["longitude"],
+                country_code=_["country"],
+                phone=_["telephone"],
+                locator_domain=locator_domain,
+                hours_of_operation="; ".join(hours),
+            )
+
+
+if __name__ == "__main__":
+    with SgWriter(
+        SgRecordDeduper(
+            RecommendedRecordIds.StoreNumberId, duplicate_streak_failure_factor=100
         )
-        location_name = grid.find("span", attrs={"itemprop": "name"}).text.strip()
-        latitude = grid.find("li")["data-latitude"]
-        longitude = grid.find("li")["data-longitude"]
-
-        full_address = unidecode.unidecode(grid.find("span", attrs={"id": "idLocalidad"}).text.strip())
-        addr = parse_address_intl(full_address)
-
-        city = addr.city
-        if city is None:
-            city = "<MISSING>"
-
-        address_1 = addr.street_address_1
-        address_2 = addr.street_address_2
-
-        if address_1 is None and address_2 is None:
-            address = "<MISSING>"
-        else:
-            address = (str(address_1) + " " + str(address_2)).strip().replace(" None", "")
-
-        state = addr.state
-        if state is None:
-            state = "<MISSING>"
-
-        zipp = addr.postcode
-        if zipp is None:
-            zipp = "<MISSING>"
-
-        country_code = "ES"
-        store_number = (
-            grid.find("p", attrs={"id": "tiendaMapaUbicacion"})
-            .find("div")["id"]
-            .replace("mapa-", "")
-        )
-
-        phone = grid.find("span", attrs={"itemprop": "telephone"}).text.strip()
-        location_type = "<MISSING>"
-
-        hours = "<MISSING>"
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "store_number": store_number,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        }
-
-
-def scrape():
-    field_defs = sp.SimpleScraperPipeline.field_definitions(
-        locator_domain=sp.MappingField(mapping=["locator_domain"]),
-        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
-        location_name=sp.MappingField(
-            mapping=["location_name"], part_of_record_identity=True
-        ),
-        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
-        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
-        street_address=sp.MultiMappingField(
-            mapping=["street_address"], is_required=False
-        ),
-        city=sp.MappingField(
-            mapping=["city"],
-        ),
-        state=sp.MappingField(mapping=["state"], is_required=False),
-        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
-        country_code=sp.MappingField(mapping=["country_code"]),
-        phone=sp.MappingField(mapping=["phone"], is_required=False),
-        store_number=sp.MappingField(
-            mapping=["store_number"], part_of_record_identity=True
-        ),
-        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
-        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
-    )
-
-    pipeline = sp.SimpleScraperPipeline(
-        scraper_name="Crawler",
-        data_fetcher=get_data,
-        field_definitions=field_defs,
-        log_stats_interval=15,
-    )
-    pipeline.run()
-
-
-scrape()
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
