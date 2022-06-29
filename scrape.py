fbd190176182ea611472f72dac4650ad7febe44d
extrasauceontop <msuchemba@gmail.com>
2022-06-29 17:21:39 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,126 +1,72 @@
+import json
+from lxml import html
 from sgscrape.sgrecord import SgRecord
-from sgscrape.sgwriter import SgWriter
 from sgrequests import SgRequests
-from sgselenium import SgChrome
-from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgrecord_deduper import SgRecordDeduper
-from sglogging import SgLogSetup
-from datetime import datetime, timedelta
-from bs4 import BeautifulSoup as bs
-import math
-from concurrent.futures import ThreadPoolExecutor
-import os
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context
-
-os.environ[
-    "PROXY_URL"
-] = "http://groups-RESIDENTIAL,country-us:{}@proxy.apify.com:8000/"
-
-logger = SgLogSetup().get_logger("quizclothing")
-_headers = {
-    "accept": "application/json",
-    "accept-language": "en-US,en;q=0.9",
-    "content-type": "application/json",
-    "origin": "https://mrbeastburger.com",
-    "referer": "https://mrbeastburger.com/",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
-}
-
-header2 = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9",
-    "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/12.0 Mobile/15A372 Safari/604.1",
-}
-
-locator_domain = "https://mrbeastburger.com"
-base_url = "https://api.dineengine.io/mrbeastburger/custom/dineengine/vendor/olo/restaurants?includePrivate=false"
-calendar_url = "https://api.dineengine.io/mrbeastburger/custom/dineengine/vendor/olo/restaurants/{}/calendars?from={}&to={}"
-today = datetime.today()
-mon = (today + timedelta(days=-today.weekday())).strftime("%Y%m%d")
-next_mon = (today + timedelta(days=-today.weekday(), weeks=1)).strftime("%Y%m%d")
-max_workers = 32
 
 
-def fetchConcurrentSingle(link):
-    page_url = locator_domain + "/locations" + link["url"].split("/menu")[-1]
-    logger.info(page_url)
-    try:
-        ca = request_with_retries(
-            calendar_url.format(link["id"], mon, next_mon)
-        ).json()["calendar"]
-    except:
-        ca = None
-    return link, page_url, ca
+def fetch_data(sgw: SgWriter):
 
+    locator_domain = "https://theoldnoconabootfactory.com/"
+    api_url = "https://www.powr.io/map/u/01008607_1602195050#platform=shopify&url=https%3A%2F%2Ftheoldnoconabootfactory.com%2Fpages%2Flocations"
+    session = SgRequests()
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0",
+    }
+    r = session.get(api_url, headers=headers)
+    print(r.text)
+    js_block = r.text.split('"locations":')[1].split(',"map_display"')[0].strip()
+    js = json.loads(js_block)
+    for j in js:
 
-def fetchConcurrentList(list, occurrence=max_workers):
-    output = []
-    total = len(list)
-    reminder = math.floor(total / 50)
-    if reminder < occurrence:
-        reminder = occurrence
-
-    count = 0
-    with ThreadPoolExecutor(
-        max_workers=occurrence, thread_name_prefix="fetcher"
-    ) as executor:
-        for result in executor.map(fetchConcurrentSingle, list):
-            if result:
-                count = count + 1
-                if count % reminder == 0:
-                    logger.debug(f"Concurrent Operation count = {count}")
-                output.append(result)
-    return output
-
-
-def request_with_retries(url):
-    return session.get(url, headers=_headers)
-
-
-def fetch_data():
-    with SgChrome(
-        user_agent="Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-        is_headless=True,
-    ) as driver:
-        driver.get(base_url)
-        locations = bs(driver.page_source, "lxml").select("restaurant")
-        for _, page_url, ca in fetchConcurrentList(locations):
-            hours = []
-            if ca:
-                for hr in ca["calendar"]:
-                    if not hr["label"]:
-                        for hh in hr["ranges"]:
-                            temp = f"{hh['weekday']}: {hh['start'].split()[-1]} - {hh['end'].split()[-1]}"
-                            if temp not in hours:
-                                hours.append(temp)
-                        break
-            yield SgRecord(
-                page_url=page_url,
-                store_number=_["id"],
-                location_name=_["name"],
-                street_address=_["streetaddress"],
-                city=_["city"],
-                state=_["state"],
-                zip_postal=_["zip"],
-                latitude=_["latitude"],
-                longitude=_["longitude"],
-                country_code=_["country"],
-                phone=_["telephone"],
-                locator_domain=locator_domain,
-                hours_of_operation="; ".join(hours),
+        ad = "".join(j.get("address"))
+        page_url = "https://theoldnoconabootfactory.com/pages/locations"
+        location_name = j.get("name") or "<MISSING>"
+        street_address = ad.split(",")[0].strip()
+        street_address_slug = street_address.split()[0].strip()
+        state = ad.split(",")[2].split()[0].strip()
+        postal = ad.split(",")[2].split()[1].strip()
+        country_code = "US"
+        city = ad.split(",")[1].strip()
+        latitude = j.get("lat") or "<MISSING>"
+        longitude = j.get("lng") or "<MISSING>"
+        r = session.get(page_url, headers=headers)
+        tree = html.fromstring(r.text)
+        phone = (
+            "".join(
+                tree.xpath(
+                    f'//p[./a[contains(text(), "{street_address_slug}")]]/following-sibling::p[1]//text()'
+                )
             )
+            or "<MISSING>"
+        )
+
+        row = SgRecord(
+            locator_domain=locator_domain,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=postal,
+            country_code=country_code,
+            store_number=SgRecord.MISSING,
+            phone=phone,
+            location_type=SgRecord.MISSING,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=SgRecord.MISSING,
+            raw_address=ad,
+        )
+
+        sgw.write_row(row)
 
 
 if __name__ == "__main__":
-    with SgRequests() as session:
-        with SgWriter(
-            SgRecordDeduper(
-                RecommendedRecordIds.StoreNumberId, duplicate_streak_failure_factor=100
-            )
-        ) as writer:
-            results = fetch_data()
-            for rec in results:
-                writer.write_row(rec)
+    session = SgRequests()
+    with SgWriter(
+        SgRecordDeduper(SgRecordID({SgRecord.Headers.RAW_ADDRESS}))
+    ) as writer:
+        fetch_data(writer)
