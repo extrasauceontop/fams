e3b099489cea795d80e73bbad3ac69620e50e507
extrasauceontop <msuchemba@gmail.com>
2022-06-30 18:52:03 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,78 +1,153 @@
-from sgrequests import SgRequests
-from sgscrape import simple_scraper_pipeline as sp
+import ssl
+import time
+
+import undetected_chromedriver as uc
+
+from bs4 import BeautifulSoup
+from sglogging import sglog
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgrecord_id import RecommendedRecordIds
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from webdriver_manager.chrome import ChromeDriverManager
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+log = sglog.SgLogSetup().get_logger(logger_name="brookshires.com")
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 
-def get_data():
-    url = "https://fastpaydayloansfloridainc.com/stores.json"
-    with SgRequests() as session:
-        response = session.get(url).json()["locations"]
+def fetch_data(sgw: SgWriter):
 
-    for location in response:
-        locator_domain = "fastpaydayloansfloridainc.com"
-        page_url = location["website"] + "/" + location["store_url"]
-        location_name = location["business_name"]
-        latitude = location["latitude"]
-        longitude = location["longitude"]
-        city = location["locality"]
-        store_number = location["store_code"]
-        address = location["address_line_1"]
-        state = location["administrative_area"]
-        zipp = location["postal_code5"]
-        phone = location["primary_phone"]
-        location_type = "<MISSING>"
-        hours = "<MISSING>"
-        country_code = "US"
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "store_number": store_number,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        }
+    base_link = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=6"
+
+    options = uc.ChromeOptions()
+    options.headless = True
+
+    with uc.Chrome(
+        driver_executable_path=ChromeDriverManager().install(), options=options
+    ) as driver:
+        for i in range(10):
+            log.info(f"Loading main page {base_link}")
+            driver.get(base_link)
+            try:
+                WebDriverWait(driver, 120).until(
+                    EC.presence_of_element_located(
+                        (By.CLASS_NAME, "store-list__scroll-container")
+                    )
+                )
+                time.sleep(10)
+                soup = BeautifulSoup(driver.page_source, "lxml")
+                grids = soup.find(
+                    "div", class_="store-list__scroll-container"
+                ).find_all("li")
+                if grids:
+                    log.info("Got Grids")
+                    break
+            except:
+                pass
+
+        for grid in grids:
+            name = grid.find("span", {"class": "store-name"}).text.strip()
+            number = grid.find(
+                "span",
+                attrs={"ng-if": "$ctrl.showStoreNumber && $ctrl.store.storeNumber"},
+            ).text.strip()
+            page_url = (
+                "https://www.brookshires.com/stores/"
+                + name.split("\n")[0].replace(" ", "-").replace(".", "").lower()
+                + "-"
+                + number.split("\n")[0].split("#")[-1]
+                + "/"
+                + grid["id"].split("-")[-1]
+            )
+            time.sleep(2)
+            for i in range(10):
+                driver.get(page_url)
+                log.info("Pull content => " + page_url)
+                try:
+                    WebDriverWait(driver, 30).until(
+                        EC.presence_of_element_located(
+                            (By.CLASS_NAME, "store-details-store-hours__content")
+                        )
+                    )
+                    break
+                except:
+                    time.sleep(10)
+
+            location_soup = BeautifulSoup(driver.page_source, "lxml")
+
+            locator_domain = "https://www.brookshires.com/"
+            location_name = location_soup.find("meta", attrs={"property": "og:title"})[
+                "content"
+            ]
+            street_address = location_soup.find(
+                "meta", attrs={"property": "og:street-address"}
+            )["content"]
+            city = location_soup.find("meta", attrs={"property": "og:locality"})[
+                "content"
+            ]
+            state = location_soup.find("meta", attrs={"property": "og:region"})[
+                "content"
+            ]
+            zip = location_soup.find("meta", attrs={"property": "og:postal-code"})[
+                "content"
+            ]
+            country_code = location_soup.find(
+                "meta", attrs={"property": "og:country-name"}
+            )["content"]
+            store_number = location_name.split("#")[-1]
+            phone = location_soup.find("meta", attrs={"property": "og:phone_number"})
+            if not phone:
+                phone = "<MISSING>"
+            else:
+                phone = phone["content"]
+            location_type = "<MISSING>"
+            latitude = location_soup.find(
+                "meta", attrs={"property": "og:location:latitude"}
+            )["content"]
+            longitude = location_soup.find(
+                "meta", attrs={"property": "og:location:longitude"}
+            )["content"]
+
+            hours = ""
+            days = location_soup.find(
+                "dl", attrs={"aria-label": "Store Hours"}
+            ).find_all("dt")
+            hours_list = location_soup.find(
+                "dl", attrs={"aria-label": "Store Hours"}
+            ).find_all("dd")
+
+            for x in range(len(days)):
+                day = days[x].text.strip()
+                hour = hours_list[x].text.strip()
+                hours = hours + day + " " + hour + ", "
+
+            hours_of_operation = hours[:-2]
+
+            sgw.write_row(
+                SgRecord(
+                    locator_domain=locator_domain,
+                    page_url=page_url,
+                    location_name=location_name,
+                    street_address=street_address,
+                    city=city,
+                    state=state,
+                    zip_postal=zip,
+                    country_code=country_code,
+                    store_number=store_number,
+                    phone=phone,
+                    location_type=location_type,
+                    latitude=latitude,
+                    longitude=longitude,
+                    hours_of_operation=hours_of_operation,
+                )
+            )
 
 
-def scrape():
-    field_defs = sp.SimpleScraperPipeline.field_definitions(
-        locator_domain=sp.MappingField(mapping=["locator_domain"]),
-        page_url=sp.MappingField(mapping=["page_url"], part_of_record_identity=True),
-        location_name=sp.MappingField(
-            mapping=["location_name"], part_of_record_identity=True
-        ),
-        latitude=sp.MappingField(mapping=["latitude"], part_of_record_identity=True),
-        longitude=sp.MappingField(mapping=["longitude"], part_of_record_identity=True),
-        street_address=sp.MultiMappingField(
-            mapping=["street_address"], is_required=False
-        ),
-        city=sp.MappingField(
-            mapping=["city"],
-        ),
-        state=sp.MappingField(mapping=["state"], is_required=False),
-        zipcode=sp.MultiMappingField(mapping=["zip"], is_required=False),
-        country_code=sp.MappingField(mapping=["country_code"]),
-        phone=sp.MappingField(mapping=["phone"], is_required=False),
-        store_number=sp.MappingField(
-            mapping=["store_number"], part_of_record_identity=True
-        ),
-        hours_of_operation=sp.MappingField(mapping=["hours"], is_required=False),
-        location_type=sp.MappingField(mapping=["location_type"], is_required=False),
-    )
-
-    pipeline = sp.SimpleScraperPipeline(
-        scraper_name="Crawler",
-        data_fetcher=get_data,
-        field_definitions=field_defs,
-        log_stats_interval=15,
-    )
-    pipeline.run()
-
-
-scrape()
+if __name__ == "__main__":
+    with SgWriter(SgRecordDeduper(RecommendedRecordIds.PageUrlId)) as writer:
+        fetch_data(writer)
