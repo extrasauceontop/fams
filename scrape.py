feaa0767991b16d98f86da5ce92d4694a0f69974
extrasauceontop <msuchemba@gmail.com>
2022-06-30 12:00:00 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,12 +1,128 @@
+from bs4 import BeautifulSoup as bs
 from sgrequests import SgRequests
+from sglogging import sglog
+from sgscrape.sgrecord import SgRecord
+from sgscrape.sgwriter import SgWriter
+from sgscrape.sgrecord_deduper import SgRecordDeduper
+from sgscrape.sgrecord_id import SgRecordID
+from sgscrape.sgpostal import parse_address_intl
+import re
 
+DOMAIN = "fastpaydayloansfloridainc.com"
+BASE_URL = "https://fastpaydayloansfloridainc.com/"
+LOCATION_URL = "https://fastpaydayloansfloridainc.com/sitemap"
+API_URL = "https://fastpaydayloansfloridainc.com/closest-stores?loan_type=all&zipcode={}&num=1"
 HEADERS = {
     "Accept": "application/json, text/plain, */*",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
 }
+MISSING = "<MISSING>"
+log = sglog.SgLogSetup().get_logger(logger_name=DOMAIN)
 
-with SgRequests() as session:
-    url = "https://fastpaydayloansfloridainc.com/sitemap"
-    response = session.get(url, headers=HEADERS).text
+session = SgRequests()
 
-print(response)
+
+def getAddress(raw_address):
+    try:
+        if raw_address is not None and raw_address != MISSING:
+            data = parse_address_intl(raw_address)
+            street_address = data.street_address_1
+            if data.street_address_2 is not None:
+                street_address = street_address + " " + data.street_address_2
+            city = data.city
+            state = data.state
+            zip_postal = data.postcode
+
+            if street_address is None or len(street_address) == 0:
+                street_address = MISSING
+            if city is None or len(city) == 0:
+                city = MISSING
+            if state is None or len(state) == 0:
+                state = MISSING
+            if zip_postal is None or len(zip_postal) == 0:
+                zip_postal = MISSING
+            return street_address, city, state, zip_postal
+    except Exception as e:
+        log.info(f"No valid address {e}")
+        pass
+    return MISSING, MISSING, MISSING, MISSING
+
+
+def pull_content(url):
+    log.info("Pull content => " + url)
+    soup = bs(session.get(url, headers=HEADERS).content, "lxml")
+    return soup
+
+
+def fetch_data():
+    log.info("Fetching store_locator data")
+    soup = pull_content(LOCATION_URL)
+    print(soup)
+    contents = soup.find("div", {"id": "leftcontent"}).find_all(
+        "a",
+        {
+            "href": re.compile(
+                r"https:\/\/fastpaydayloansfloridainc.com\/florida-payday-loan-locations.*"
+            )
+        },
+    )
+    for row in contents:
+        url = row["href"]
+        zip_code = url.split("/")[-1]
+        stores = session.get(API_URL.format(zip_code), headers=HEADERS).json()
+        data = stores["locations"][0]
+        store_number = data["store_code"]
+        page_url = BASE_URL + stores["storeUrls"][store_number]
+        location_name = data["business_name"]
+        street_address = data["address_line_1"]
+        city = data["locality"]
+        state = data["administrative_area"]
+        zip_postal = data["postal_code"]
+        phone = data["primary_phone"]
+        hours_of_operation = MISSING
+        country_code = "US"
+        location_type = "paydayloans"
+        latitude = data["latitude"]
+        longitude = data["longitude"]
+        log.info("Append {} => {}".format(location_name, street_address))
+        yield SgRecord(
+            locator_domain=DOMAIN,
+            page_url=page_url,
+            location_name=location_name,
+            street_address=street_address,
+            city=city,
+            state=state,
+            zip_postal=zip_postal,
+            country_code=country_code,
+            store_number=store_number,
+            phone=phone,
+            location_type=location_type,
+            latitude=latitude,
+            longitude=longitude,
+            hours_of_operation=hours_of_operation,
+            raw_address=f"{street_address}, {city}, {state}, {zip_postal}",
+        )
+
+
+def scrape():
+    log.info("start {} Scraper".format(DOMAIN))
+    count = 0
+    with SgWriter(
+        SgRecordDeduper(
+            SgRecordID(
+                {
+                    SgRecord.Headers.PAGE_URL,
+                }
+            )
+        )
+    ) as writer:
+        results = fetch_data()
+        for rec in results:
+            writer.write_row(rec)
+            count = count + 1
+
+    log.info(f"No of records being processed: {count}")
+    log.info("Finished")
+
+
+scrape()
