bfb0e3a668ab965dcd2411d906f59822304cec62
extrasauceontop <msuchemba@gmail.com>
2022-06-30 17:55:17 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -57,14 +57,13 @@ def pull_content(url):
 def fetch_data():
     log.info("Fetching store_locator data")
     soup = pull_content(LOCATION_URL)
-    print(soup)
     contents = ["https://fastpaydayloansfloridainc.com" + a_tag["href"] for a_tag in soup.find("div", {"id": "leftcontent"}).find_all("a") if "florida-payday-loan-locations" in a_tag["href"]]
     for url in contents:
         print(url)
         zip_code = url.split("/")[-1]
         stores = session.get(API_URL.format(zip_code), headers=HEADERS)
         print(stores)
-        print(stores.json())
+        print(stores.response.test)
         stores = stores.json()
         data = stores["locations"][0]
         store_number = data["store_code"]
