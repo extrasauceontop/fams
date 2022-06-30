c10b429e6b03543332ef772f6dfbbc45d2026261
extrasauceontop <msuchemba@gmail.com>
2022-06-30 11:40:12 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,7 +1,12 @@
 from sgrequests import SgRequests
 
+HEADERS = {
+    "Accept": "application/json, text/plain, */*",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36",
+}
+
 with SgRequests() as session:
     url = "https://fastpaydayloansfloridainc.com/sitemap"
-    response = session.get(url).text
+    response = session.get(url, headers=HEADERS).text
 
 print(response)
