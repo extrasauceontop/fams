559f88d3fe7132ee3280168ca3061a11561d264f
extrasauceontop <msuchemba@gmail.com>
2022-07-01 10:49:42 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 from sgselenium import SgChrome
 from proxyfier import ProxyProviders
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=6"
 if __name__ == "__main__":
     with SgChrome(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, block_third_parties=True) as driver:
