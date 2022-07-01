a5670dd6983044a98929c5e6710a66fcad74520e
extrasauceontop <msuchemba@gmail.com>
2022-07-01 12:30:05 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,7 +1,7 @@
 from sgselenium import SgFirefox
 from proxyfier import ProxyProviders
 
-with SgFirefox(is_headless=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, block_third_parties=False) as driver:
+with SgFirefox(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, block_third_parties=False) as driver:
     url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=6"
     driver.get(url)
     response = driver.page_source
