7dfd98fa15cfa00364e4736791398fa4f8d78f3c
extrasauceontop <msuchemba@gmail.com>
2022-07-01 11:56:30 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -10,6 +10,7 @@ def fetch_data(sgw: SgWriter):
     api = "https://www.rewe.de/market/content/marketsearch"
 
     for i in range(100):
+        print(i)
         data = f'-----------------------------335590349241424249481678125346\r\nContent-Disposition: form-data; name="searchString"\r\n\r\n*\r\n-----------------------------335590349241424249481678125346\r\nContent-Disposition: form-data; name="page"\r\n\r\n{i}\r\n-----------------------------335590349241424249481678125346\r\nContent-Disposition: form-data; name="city"\r\n\r\n\r\n-----------------------------335590349241424249481678125346\r\nContent-Disposition: form-data; name="pageSize"\r\n\r\n50\r\n-----------------------------335590349241424249481678125346--\r\n'
         r = session.post(api, headers=headers, data=data)
         try:
