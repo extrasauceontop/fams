c25ec4c8308a7fef2ab3e307e99fa2089eec4b0c
extrasauceontop <msuchemba@gmail.com>
2022-07-03 15:30:17 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -5,15 +5,25 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs
 
-url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=8"
-with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
-    driver.get(url)
-    WebDriverWait(driver, 120).until(
-        EC.presence_of_element_located(
-            (By.CLASS_NAME, "address1")
-        )
-    )
-    response = driver.page_source
+x = 0
+while True:
+    x = x+1
+    if x == 10:
+        raise Exception
+    url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=8"
+    try:
+        with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
+            driver.get(url)
+            WebDriverWait(driver, 120).until(
+                EC.presence_of_element_located(
+                    (By.CLASS_NAME, "address1")
+                )
+            )
+            response = driver.page_source
+            break
+    
+    except Exception:
+        continue
 
 soup = bs(response, "html.parser")
 grids = soup.find_all("store-details-preview", attrs={"aria-controls": "store-details-pane"})
