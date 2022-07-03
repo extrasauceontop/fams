76a79ef9955fbb0060a7828e005198440f6e38ec
extrasauceontop <msuchemba@gmail.com>
2022-07-03 14:05:22 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -5,7 +5,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=8"
-with SgFirefox(is_headless=True, block_third_parties=True, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
+with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
     driver.get(url)
     WebDriverWait(driver, 120).until(
         EC.presence_of_element_located(
