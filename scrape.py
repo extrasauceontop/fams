eda158a197b34c6a0eef80e3b8319c931c24531d
extrasauceontop <msuchemba@gmail.com>
2022-07-03 14:37:25 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -3,6 +3,7 @@ from proxyfier import ProxyProviders
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as bs
 
 url = "https://www.brookshires.com/stores/?coordinates=39.84686380709379,-106.87749199999999&zoom=8"
 with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
@@ -13,6 +14,10 @@ with SgFirefox(is_headless=True, block_third_parties=False, proxy_provider_escal
         )
     )
     response = driver.page_source
+
+soup = bs(response, "html.parser")
+grids = soup.find_all("div", attrs={"arie-controls": "store-details-pane"})
+print(len(grids))
 
 
 print(response)
