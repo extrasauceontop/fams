42373af79f901f931c21d0a55fa34b694d42467a
extrasauceontop <msuchemba@gmail.com>
2022-07-05 15:12:09 -0500

    scrape

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from sgselenium import SgChrome
 from proxyfier import ProxyProviders
+import ssl
 
+ssl._create_default_https_context = ssl._create_unverified_context
 if __name__ == "__main__":
     url = "https://nomnom-prod-api.pieology.com/restaurants/near?lat=35.7796&long=-78.6382&radius=100&limit=500&nomnom=calendars&nomnomcalendar_from=20220705&nomnom_calendars_to=20220711&nomnom_exclude_extref=999"
     with SgChrome(block_third_parties=False, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) as driver:
