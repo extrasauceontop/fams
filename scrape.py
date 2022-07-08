b902e3bb2e9bedccdf99cb11df2cd025bc5e25b6
extrasauceontop <msuchemba@gmail.com>
2022-07-08 13:51:31 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,11 +1,10 @@
 from sgselenium import SgFirefox
 import time
 
-url = "https://www.bravissimo.com/us/shops/all/"
+url = "https://www.galeria.de/filialen/l"
 with SgFirefox(is_headless=True) as driver:
     driver.get(url)
     time.sleep(10)
     response = driver.page_source
-
 
 print(response)
