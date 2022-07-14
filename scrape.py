f4b3813f2beadcbfc5537780bb51a2ada31e1753
extrasauceontop <msuchemba@gmail.com>
2022-07-14 16:27:25 -0500

    scrap

--- scrape.py
+++ scrape.py
@@ -105,7 +105,7 @@ def get_data():
             location_type = "<MISSING>"
             store_number = "<MISSING>"
 
-            if hours == "<MISSING>":
+            if "<MISSING>" in hours:
                 hours_json = extract_json(page_response.split("openingHoursSpecification")[1])
                 print(hours_json)
                 print("")
