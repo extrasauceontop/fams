96479aa9bab62303e1d4c3276506cb1d32a9ffeb
extrasauceontop <msuchemba@gmail.com>
2022-07-14 12:20:08 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -9,75 +9,75 @@ def get_data():
     with SgRequests() as session:
         response = session.get(url).text.replace("<br>", "\n")
 
-    soup = bs(response, "html.parser")
-    grids = soup.find_all("div", attrs={"class": "store"})
+        soup = bs(response, "html.parser")
+        grids = soup.find_all("div", attrs={"class": "store"})
 
-    for grid in grids:
-        locator_domain = "razer.com"
-        page_url = "https://www.razer.com" + grid.find("a", attrs={"class": "gtm_learn_more"})["href"][1:]
-        location_name = grid.find("div", attrs={"class": "address"}).find("strong").text.strip().replace("\n", " ")
+        for grid in grids:
+            locator_domain = "razer.com"
+            page_url = "https://www.razer.com" + grid.find("a", attrs={"class": "gtm_learn_more"})["href"][1:]
+            location_name = grid.find("div", attrs={"class": "address"}).find("strong").text.strip().replace("\n", " ")
 
-        address_parts = grid.find("div", attrs={"class": "address"}).find("p")
-        first_line = address_parts.find("span").text.strip()
-        full_address = address_parts.text.strip()
-        raw_address = unidecode.unidecode(full_address.replace(first_line, first_line + " ")).replace("\n", " ").replace("\t", "").replace("\r", "")
-        while "  " in raw_address:
-            raw_address = raw_address.replace("  ", " ")
+            address_parts = grid.find("div", attrs={"class": "address"}).find("p")
+            first_line = address_parts.find("span").text.strip()
+            full_address = address_parts.text.strip()
+            raw_address = unidecode.unidecode(full_address.replace(first_line, first_line + " ")).replace("\n", " ").replace("\t", "").replace("\r", "")
+            while "  " in raw_address:
+                raw_address = raw_address.replace("  ", " ")
 
-        addr = parse_address_intl(full_address)
-        city = addr.city
-        if city is None:
-            city = "<MISSING>"
+            addr = parse_address_intl(full_address)
+            city = addr.city
+            if city is None:
+                city = "<MISSING>"
 
-        address_1 = addr.street_address_1
-        address_2 = addr.street_address_2
+            address_1 = addr.street_address_1
+            address_2 = addr.street_address_2
 
-        if address_1 is None and address_2 is None:
-            address = "<MISSING>"
-        else:
-            address = (str(address_1) + " " + str(address_2)).strip()
+            if address_1 is None and address_2 is None:
+                address = "<MISSING>"
+            else:
+                address = (str(address_1) + " " + str(address_2)).strip()
 
-        state = addr.state
-        if state is None:
-            state = "<MISSING>"
+            state = addr.state
+            if state is None:
+                state = "<MISSING>"
 
-        zipp = addr.postcode
-        if zipp is None:
-            zipp = "<MISSING>"
+            zipp = addr.postcode
+            if zipp is None:
+                zipp = "<MISSING>"
 
-        country_code = addr.country
-        if country_code is None:
-            country_code = "<MISSING>"
+            country_code = addr.country
+            if country_code is None:
+                country_code = "<MISSING>"
 
-        page_response = session.get(page_url).text
-        page_soup = bs(page_response, "html.parser")
+            page_response = session.get(page_url).text
+            page_soup = bs(page_response, "html.parser")
 
-        latitude = page_response.split("2!3d")[1].split("!")[0]
-        longitude = page_response.split("2!3d")[1].split("!4d")[1].split('"')[0]
+            latitude = page_response.split("2!3d")[1].split("!")[0]
+            longitude = page_response.split("2!3d")[1].split("!4d")[1].split('"')[0]
 
-        phone = "<LATER>"
-        hours = "<LATER>"
+            phone = "<LATER>"
+            hours = "<LATER>"
 
-        address = address.replace(" None", "")
-        location_type = "<MISSING>"
-        store_number = "<MISSING>"
+            address = address.replace(" None", "")
+            location_type = "<MISSING>"
+            store_number = "<MISSING>"
 
-        yield {
-            "locator_domain": locator_domain,
-            "page_url": page_url,
-            "location_name": location_name,
-            "latitude": latitude,
-            "longitude": longitude,
-            "city": city,
-            "street_address": address,
-            "state": state,
-            "zip": zipp,
-            "store_number": store_number,
-            "phone": phone,
-            "location_type": location_type,
-            "hours": hours,
-            "country_code": country_code,
-        }
+            yield {
+                "locator_domain": locator_domain,
+                "page_url": page_url,
+                "location_name": location_name,
+                "latitude": latitude,
+                "longitude": longitude,
+                "city": city,
+                "street_address": address,
+                "state": state,
+                "zip": zipp,
+                "store_number": store_number,
+                "phone": phone,
+                "location_type": location_type,
+                "hours": hours,
+                "country_code": country_code,
+            }
 
 
 def scrape():
