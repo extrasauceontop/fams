8a45e7b49f11ea10ef488f24a149b96b0fb4bf37
extrasauceontop <msuchemba@gmail.com>
2022-07-14 16:01:47 -0500

    scrap

--- test.py
+++ test.py
@@ -2,7 +2,6 @@ from sgrequests import SgRequests
 from bs4 import BeautifulSoup as bs
 from sgscrape import simple_scraper_pipeline as sp
 import unidecode
-from sgpostal.sgpostal import parse_address_intl
 import json
 
 
@@ -24,7 +23,9 @@ def extract_json(html_string):
                 end = count
                 try:
                     json_objects.append(json.loads(html_string[start : end + 1]))
-                except Exception:
+                except Exception as e:
+                    print(html_string[start : end + 1])
+                    print(e)
                     pass
         count = count + 1
 
@@ -40,72 +41,23 @@ def get_data():
         grids = soup.find_all("div", attrs={"class": "store"})
 
         for grid in grids:
-            phone = "<MISSING>"
-            hours = "<MISSING>"
             locator_domain = "razer.com"
             page_url = "https://www.razer.com" + grid.find("a", attrs={"class": "gtm_learn_more"})["href"][1:]
             location_name = grid.find("div", attrs={"class": "address"}).find("strong").text.strip().replace("\n", " ")
 
-            address_parts = grid.find("div", attrs={"class": "address"}).find("p")
-            first_line = address_parts.find("span").text.strip()
-            full_address = address_parts.text.strip()
-            raw_address = unidecode.unidecode(full_address.replace(first_line, first_line + " ")).replace("\n", " ").replace("\t", "").replace("\r", "")
-            while "  " in raw_address:
-                raw_address = raw_address.replace("  ", " ")
-
-            addr = parse_address_intl(full_address)
-            city = addr.city
-            if city is None:
-                city = "<MISSING>"
-
-            address_1 = addr.street_address_1
-            address_2 = addr.street_address_2
-
-            if address_1 is None and address_2 is None:
-                address = "<MISSING>"
-            else:
-                address = (str(address_1) + " " + str(address_2)).strip()
-
-            state = addr.state
-            if state is None:
-                state = "<MISSING>"
-
-            zipp = addr.postcode
-            if zipp is None:
-                zipp = "<MISSING>"
-
-            country_code = addr.country
-            if country_code is None:
-                country_code = "<MISSING>"
+            
 
             print(page_url)
             page_response = session.get(page_url).text.replace("<br>", "\n")
-            page_soup = bs(page_response, "html.parser")
+            
+            page_json = extract_json(page_response.split("og:type")[1])
 
-            try:
-                latitude = page_response.split("2!3d")[1].split("!")[0]
-                longitude = page_response.split("2!3d")[1].split("!4d")[1].split('"')[0]
-            except Exception:
-                latitude = "<MISSING>"
-                longitude = "<MISSING>"
+            with open("file.txt", "w", encoding="utf-8") as output:
+                json.dump(page_json, output, indent=4)
 
-            phone_parts = page_soup.find_all("p", attrs={"class": "p-container lt1"})
-            for part in phone_parts:
-                if "+" in part.text.strip():
-                    phone = part.text.strip().split("+")[1].split("\n")[0]
-                    
-                    hours = ""
-                    for line in part.text.strip().split("\n"):
-                        if "AM" in line and "PM" in line:
-                            hours = hours + line + ", "
-                        
-                    break
-            print(phone)
 
-            address = address.replace(" None", "")
-            location_type = "<MISSING>"
-            store_number = "<MISSING>"
-            hours = hours[:-2]
+
+            
 
             yield {
                 "locator_domain": locator_domain,
