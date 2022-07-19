5516e4c71d8d91abf908f8a8ec9c42a3626e088c
extrasauceontop <msuchemba@gmail.com>
2022-07-19 12:31:35 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,61 +1,68 @@
-from requests_toolbelt import MultipartEncoder
-from urllib.parse import urljoin
+# -*- coding: utf-8 -*-
+# --extra-index-url https://dl.cloudsmith.io/KVaWma76J5VNwrOm/crawl/crawl/python/simple/
+import ssl
+import pandas as pd
 
-from sgrequests import SgRequests
 from sgscrape.sgrecord import SgRecord
 from sgscrape.sgrecord_deduper import SgRecordDeduper
 from sgscrape.sgrecord_id import SgRecordID
 from sgscrape.sgwriter import SgWriter
-from sgpostal.sgpostal import parse_address_intl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 
 def fetch_data():
-    session = SgRequests()
-
-    start_url = "https://www.columbia.hu/?ajax_event=load_shop_finder_data"
-    domain = "columbia.hu"
-
-    frm = {"ajax_event": "load_shop_finder_data"}
-    me = MultipartEncoder(fields=frm)
-    me_boundary = me.boundary[2:]
-    me_body = me.to_string()
-    hdr = {
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Accept-Language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7,pt;q=0.6",
-        "Content-Type": "multipart/form-data; boundary=" + me_boundary,
-        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36",
-        "X-Requested-With": "XMLHttpRequest",
+    start_urls = {
+        "https://www.sony.com.ar/corporate/AR/dondecomprar/dondecomprar.html": "https://www.sony.com.ar/corporate/AR/dondecomprar/data/AR_Stores.xls",
+        "https://www.sony.co.cr/corporate/CR/dondecomprar/dondecomprar.html": "https://www.sony.co.cr/corporate/CR/dondecomprar/data/CR_Stores.xls",
+        "https://www.sony.com.mx/corporate/MX/dondecomprar/dondecomprar.html": "https://www.sony.com.mx/corporate/MX/dondecomprar/data/MX_Stores.xlsx",
+        "https://www.sony.com.do/corporate/DO/dondecomprar/dondecomprar.html": "https://www.sony.com.do/corporate/DO/dondecomprar/data/DO_Stores.xlsx",
+        "https://www.sony.com.bo/corporate/BO/dondecomprar/dondecomprar.html": "https://www.sony.com.bo/corporate/BO/dondecomprar/data/BO_Stores.xls",
+        "https://www.sony.com.ec/corporate/EC/dondecomprar/dondecomprar.html": "https://www.sony.com.ec/corporate/EC/dondecomprar/data/EC_Stores.xls",
+        "https://www.sony.com.ni/corporate/NI/dondecomprar/dondecomprar.html": "https://www.sony.com.ni/corporate/NI/dondecomprar/data/NI_Stores.xlsx",
+        "https://www.sony.com.sv/corporate/SV/dondecomprar/dondecomprar.html": "https://www.sony.com.sv/corporate/SV/dondecomprar/data/SV_Stores.xls",
+        "https://www.sony.com.pa/corporate/PA/dondecomprar/dondecomprar.html": "https://www.sony.com.pa/corporate/PA/dondecomprar/data/PA_Stores.xls",
+        "https://www.sony.cl/corporate/CL/dondecomprar/dondecomprar.html": "https://www.sony.cl/corporate/CL/dondecomprar/data/CL_Stores.xls",
+        "https://www.sony.com.gt/corporate/GT/dondecomprar/dondecomprar.html": "https://www.sony.com.gt/corporate/GT/dondecomprar/data/GT_Stores.xls",
+        "https://www.sony.com.pe/corporate/PE/dondecomprar/dondecomprar.html": "https://www.sony.com.pe/corporate/PE/dondecomprar/data/PE_Stores.xlsx",
+        "https://www.sony.com.co/corporate/CO/dondecomprar/dondecomprar.html": "https://www.sony.com.co/corporate/CO/dondecomprar/data/CO_Stores.xls",
+        "https://www.sony.com.hn/corporate/HN/dondecomprar/dondecomprar.html": "https://www.sony.com.hn/corporate/HN/dondecomprar/data/HN_Stores.xls",
     }
-    data = session.post(start_url, data=me_body, headers=hdr).json()
-    for poi in data["shops"]:
-        page_url = urljoin(start_url, poi["url"])
-        raw_address = poi["address"]
-        addr = parse_address_intl(raw_address)
-        street_address = addr.street_address_1
-        if addr.street_address_2:
-            street_address += " " + addr.street_address_2
-        hoo = " ".join(poi["open"].split()).replace("$", " ").replace("|", " ")
+    domain = "sony-lain.com"
 
-        item = SgRecord(
-            locator_domain=domain,
-            page_url=page_url,
-            location_name=poi["label"],
-            street_address=street_address,
-            city=addr.city,
-            state="",
-            zip_postal=raw_address.split()[0],
-            country_code="",
-            store_number=poi["id"],
-            phone=poi["phone"],
-            location_type="",
-            latitude=poi["gps"].split(",")[0],
-            longitude=poi["gps"].split(",")[1],
-            hours_of_operation=hoo,
-            raw_address=poi["address"],
-        )
+    for page_url, data_url in start_urls.items():
+        try:
+            df = pd.read_excel(data_url)
+        except Exception:
+            df = pd.read_excel(data_url, engine='openpyxl')
+        for index, poi in df.iterrows():
+            phone = ""
+            if type(poi["phone"]) != float:
+                phone = str(poi["phone"]).split(",")[0]
+            print(poi["name"])
+            item = SgRecord(
+                locator_domain=domain,
+                page_url=page_url,
+                location_name=poi["name"],
+                street_address=poi["address"],
+                city=poi["city"],
+                state=poi["region"],
+                zip_postal="",
+                country_code=page_url.split("/")[4],
+                store_number=index,
+                phone=phone,
+                location_type=poi["type"],
+                latitude=poi["lat"],
+                longitude=poi["lng"],
+                hours_of_operation="",
+            )
 
-        yield item
+            yield item
 
 
 def scrape():
