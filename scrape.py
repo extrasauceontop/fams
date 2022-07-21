cfd9507e8c6c4d90212ede3d4b83f163fa603bad
extrasauceontop <msuchemba@gmail.com>
2022-07-21 16:41:02 -0500

    scrape

--- scrape.py
+++ scrape.py
@@ -1,7 +1,19 @@
-from sgselenium import MagiConfig
+from sgselenium import SgFirefox
 from proxyfier import ProxyProviders
 
+if __name__ == "__main__":
+    
+    def check_response(request):
+        response = driver.page_source
+        if len(response.split("div")) > 2:
+            return True
+        
+        else:
+            return False
 
-url = "https://www.hoogvliet.com/"
-MagiConfig.find_sgselenium(url, proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER) 
+    url = "https://www.hoogvliet.com/"
+    with SgFirefox(proxy_provider_escalation_order=ProxyProviders.TEST_PROXY_ESCALATION_ORDER, is_headless=False) as driver:
+        driver.get(url)
+        response = driver.page_source
 
+    print(response)
